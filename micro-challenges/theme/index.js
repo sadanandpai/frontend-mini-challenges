40768894babe248b55614ec306831e275934c65c
Sadanand Pai <sadanand.pai@coindcx.com>
2022-01-18 23:44:16 +0530

    Light & Dark mode

---
+++ micro-challenges/theme/index.js
@@ -0,0 +1,12 @@
+const themeSwitch = document.getElementById("themeSwitch");
+const innnerThemeSwitch = document.getElementById("innnerThemeSwitch");
+
+themeSwitch.addEventListener("change", function () {
+  if (themeSwitch.checked) {
+    document.body.classList.add("dark");
+    innnerThemeSwitch.checked = true;
+  } else {
+    document.body.classList.remove("dark");
+    innnerThemeSwitch.checked = false;
+  }
+});
