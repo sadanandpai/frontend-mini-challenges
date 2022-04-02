98c6ec1cca402010e93cb8004b09464a718f47f0
Sadanand Pai <sadanand.pai@NTPL-ITMACLAP0235.local>
2022-04-02 16:41:08 +0530

    UI revamp & tele formatter

---
+++ index.js
@@ -0,0 +1,26 @@
+const challenges = [
+  { title: "Toast Popup", link: "toast-popup" },
+  { title: "Guess the number", link: "guess-the-number" },
+  { title: "Telephone formatter", link: "telephone-formatter" },
+  { title: "Light & Dark mode", link: "theme" },
+  { title: "Todo List", link: "todo-list" },
+  { title: "Star Rating", link: "star-rating" },
+  { title: "Pixel Art", link: "pixel-art" },
+  { title: "Countdown Timer", link: "timer" },
+  { title: "Carousel", link: "carousel" },
+  { title: "Paginator", link: "paginator" },
+  { title: "Comment box", link: "comment-box" },
+  { title: "Array methods", link: "array-methods" },
+];
+
+const createAnchorElement = (obj) => {
+  const a = document.createElement("a");
+  a.textContent = obj.title;
+  a.href = `./micro-challenges/${obj.link}/`;
+  return a;
+};
+
+const challengeGridEl = document.getElementById("challengeGrid");
+challenges
+  .map(createAnchorElement)
+  .forEach((el) => challengeGridEl.appendChild(el));
---
+++ micro-challenges/telephone-formatter/index.js
@@ -0,0 +1,16 @@
+const phone = document.getElementById("phone");
+
+const getStringWithNumbersOnly = (str) =>
+  [...str].filter((v) => Number.isInteger(+v) && v !== " ").join("");
+
+const formatString = (str) => {
+  const numStr = getStringWithNumbersOnly(str);
+  return numStr.length > 3
+    ? "+(" + numStr.substring(0, 3) + ") - " + numStr.substring(3)
+    : numStr;
+};
+
+phone.addEventListener(
+  "input",
+  () => (phone.value = formatString(phone.value))
+);
