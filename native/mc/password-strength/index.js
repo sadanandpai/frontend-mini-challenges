e8a103b4726dac8c693434ce1e1518d131fbc586
Sadanand Pai <sadanand.pai@coindcx.com>
2023-06-23 10:30:45 +0530

    Password strength refactor

--- native/mc/password-strength/index.js
+++ native/mc/password-strength/index.js
@@ -1,31 +1,65 @@
-const password = document.getElementById('password');
+const passwordInput = document.getElementById('password');
 const progressBar = document.getElementById('progressBar');
+const strengthEl = document.getElementById('strength');
+const lcEl = document.getElementById('lc');
+const ucEl = document.getElementById('uc');
+const numEl = document.getElementById('num');
+const symEl = document.getElementById('sym');
+const charsEl = document.getElementById('chars');
 
-password.addEventListener('input', function () {
-  // check if password contains at least one number
-  const hasNumber = /\d/;
-  // check if password contains at least one uppercase letter
-  const hasUpperCase = /[A-Z]/;
-  // check if password contains at least one lowercase letter
-  const hasLowerCase = /[a-z]/;
-  // check if password contains at least one special character
-  const hasSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+const passwordStrengths = [
+  { difficulty: 'Weak', color: 'red' },
+  { difficulty: 'Medium', color: 'orange' },
+  { difficulty: 'Strong', color: 'green' },
+];
 
-  const value = password.value;
+const hasNumber = /\d/; // check if password contains at least one number
+const hasUpperCase = /[A-Z]/; // check if password contains at least one uppercase letter
+const hasLowerCase = /[a-z]/; // check if password contains at least one lowercase letter
+const hasSpecial = /[^A-Za-z0-9]/; // check if password contains at least one special character
 
-  // check strength of the password
-  let strength = Math.min(6, Math.floor(value.length / 3));
-  strength +=
-    value.length > 3
-      ? hasNumber.test(value) + hasUpperCase.test(value) + hasLowerCase.test(value) + hasSpecial.test(value)
-      : 0;
-
-  progressBar.style.width = strength * 10 + '%';
+function getPasswordStrength(strength) {
   if (strength > 8) {
-    progressBar.style.backgroundColor = 'green';
-  } else if (strength > 5) {
-    progressBar.style.backgroundColor = 'orange';
-  } else {
-    progressBar.style.backgroundColor = 'red';
+    return passwordStrengths[2];
   }
+
+  if (strength > 5) {
+    return passwordStrengths[1];
+  }
+
+  return passwordStrengths[0];
+}
+
+function getPasswordScore(text) {
+  let score = 0;
+  if (text.length > 3) {
+    score = Math.min(6, Math.floor(text.length / 3));
+    score += hasNumber.test(text) + hasUpperCase.test(text) + hasLowerCase.test(text) + hasSpecial.test(text);
+  }
+  return score;
+}
+
+function updateUI(strength, score, length, indicators) {
+  strengthEl.textContent = strength.difficulty;
+  progressBar.style.backgroundColor = strength.color;
+  progressBar.style.width = score * 10 + '%';
+  lcEl.className = indicators.lc;
+  ucEl.className = indicators.uc;
+  numEl.className = indicators.num;
+  symEl.className = indicators.sym;
+  charsEl.textContent = length;
+}
+
+passwordInput.addEventListener('input', function () {
+  const password = passwordInput.value;
+  const score = getPasswordScore(passwordInput.value);
+  const strength = getPasswordStrength(score);
+  const [lc, uc, num, sym] = [
+    hasLowerCase.test(password),
+    hasUpperCase.test(password),
+    hasNumber.test(password),
+    hasSpecial.test(password),
+  ];
+
+  updateUI(strength, score, password.length, { lc, uc, num, sym });
 });
