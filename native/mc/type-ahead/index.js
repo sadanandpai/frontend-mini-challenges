3e3b8598e4b246534e3a3a205fc43310d3d20e32
Sadanand Pai <sadanand.pai@coindcx.com>
2023-06-24 10:21:41 +0530

    Autocomplete offline completion

--- native/mc/type-ahead/index.js
+++ native/mc/type-ahead/index.js
@@ -1,9 +1,10 @@
 import { Trie } from './trie.js';
 import { suggestionsList } from './list.js';
 
-const suggestionLength = 8;
+const suggestionLength = 5;
 const userInput = document.getElementById('userInput');
 const suggestionsEl = document.querySelector('.suggestions');
+const trie = new Trie(suggestionsList);
 let suggestionFocus = null;
 let userText = '';
 
@@ -13,21 +14,24 @@ function highlightSuggestion(index) {
   userInput.value = suggestionsEl.childNodes[index].textContent;
 }
 
-function resetSuggestion() {
+function removeHighlightedSuggestion() {
   suggestionsEl.querySelector('.highlight')?.classList.remove('highlight');
   userInput.value = userText;
   suggestionFocus = null;
 }
 
-const trie = new Trie(suggestionsList);
+function resetSuggestions() {
+  suggestionsEl.innerHTML = '';
+  suggestionFocus = null;
+  userInput.value = userText;
+}
 
 userInput.addEventListener('input', () => {
   suggestionsEl.innerHTML = '';
   suggestionFocus = null;
   userText = userInput.value;
 
-  const searchText = userInput.value;
-  const result = searchText ? trie.getWordsFromTrie(searchText, suggestionLength) : [];
+  const result = userText ? trie.getWordsFromTrie(userText, suggestionLength) : [];
   for (let i = 0; i < result.length; i++) {
     const li = document.createElement('li');
     li.textContent = result[i] ?? '';
@@ -35,48 +39,44 @@ userInput.addEventListener('input', () => {
   }
 });
 
-userInput.addEventListener('keydown', event => {
+userInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter' && suggestionFocus != null) {
     userText = suggestionsEl.childNodes[suggestionFocus].textContent;
-    userInput.value = userText;
-    suggestionFocus = null;
-    suggestionsEl.innerHTML = '';
+    resetSuggestions();
     return;
   }
 
   if (event.key === 'ArrowDown') {
     suggestionFocus = suggestionFocus ?? -1;
-
     if (suggestionFocus + 1 >= suggestionsEl.childElementCount) {
-      resetSuggestion();
+      removeHighlightedSuggestion();
       return;
     }
 
-    suggestionFocus += 1;
-    highlightSuggestion(suggestionFocus);
+    highlightSuggestion(++suggestionFocus);
   }
 
   if (event.key === 'ArrowUp') {
     event.preventDefault(); // prevents cursor moving to start of input field
     suggestionFocus = suggestionFocus ?? suggestionsEl.childElementCount;
-
     if (suggestionFocus - 1 < 0) {
-      resetSuggestion();
+      removeHighlightedSuggestion();
       return;
     }
 
-    suggestionFocus -= 1;
-    highlightSuggestion(suggestionFocus);
+    highlightSuggestion(--suggestionFocus);
   }
 });
 
-suggestionsEl.addEventListener('click', event => {
+userInput.addEventListener('blur', () => {
+  setTimeout(resetSuggestions, 100);
+});
+
+suggestionsEl.addEventListener('click', (event) => {
   const element = event.target;
 
   if (element.tagName.toLowerCase() === 'li') {
     userText = element.textContent;
-    userInput.value = userText;
-    suggestionFocus = null;
-    suggestionsEl.innerHTML = '';
+    resetSuggestions();
   }
 });
