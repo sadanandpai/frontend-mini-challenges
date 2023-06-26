bb31d89d5d686b811990f303ff186db77a1a4868
Sadanand Pai <sadanand.pai@coindcx.com>
2023-06-26 19:18:14 +0530

    React: Minor refactor - Star rating

--- react/src/machine-coding/star-rating/StarRating.jsx
+++ react/src/machine-coding/star-rating/StarRating.jsx
@@ -1,34 +1,33 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
+
 import Star from "./Star";
 
 const StarRating = ({ value, total }) => {
   const [rating, setRating] = useState(value || 0);
   const [selection, setSelection] = useState(0);
+  const starArray = useMemo(() => Array.from({ length: total }), [total]);
 
   const onHover = (event) => {
-    let val = 0;
-    if (event && event.target && event.target.getAttribute("data-star-id")) {
-      val = event.target.getAttribute("data-star-id");
-    }
-    setSelection(val);
+    setSelection(event.target.dataset?.starId ?? 0);
   };
+
+  const onLeave = () => {
+    setSelection(0);
+  };
+
+  const onClick = (event) => {
+    setRating(event.target.dataset?.starId ?? rating);
+  };
+
   return (
-    <div
-      onMouseLeave={() => onHover(null)}
-      onMouseOver={onHover}
-      onClick={(e) =>
-        setRating(e.target.getAttribute("data-star-id") || rating)
-      }
-    >
-      {Array.from({ length: total }, (_, index) => {
-        return (
-          <Star
-            marked={selection ? selection >= index + 1 : rating >= index + 1}
-            starId={index + 1}
-            key={`star_${index}`}
-          />
-        );
-      })}
+    <div onMouseLeave={onLeave} onMouseOver={onHover} onClick={onClick}>
+      {starArray.map((_, index) => (
+        <Star
+          marked={(selection || rating) > index}
+          starId={index + 1}
+          key={`star_${index + 1}`}
+        />
+      ))}
     </div>
   );
 };
