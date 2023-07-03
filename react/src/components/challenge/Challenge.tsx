1c5829df50ff39d816a700843ce87d5192e8ebf9
Sadanand Pai <sadanand.pai@coindcx.com>
2023-07-03 09:38:43 +0530

    React: 4: Refactor

--- react/src/components/challenge/Challenge.tsx
+++ react/src/components/challenge/Challenge.tsx
@@ -1,10 +1,10 @@
 import Accordion from "@/machine-coding/accordion/App";
 import Counter from "@/machine-coding/counter/counter";
+import GuessNumber from "../../machine-coding/Guess-the-number/App";
 import Header from "../header/header";
 import LightDarkMode from "@/machine-coding/light-dark-mode/App";
 import StarRating from "@/machine-coding/star-rating/App";
 import TelephoneFormatter from "@/machine-coding/telephone-formatter";
-import GuessNumber from  "../../machine-coding/Guess-the-number/App";
 import { useParams } from "react-router-dom";
 
 const reactChallenges = [
@@ -12,7 +12,7 @@ const reactChallenges = [
   <Accordion />,
   <StarRating />,
   <LightDarkMode />,
-  <GuessNumber/>,  // Gap for "Guess the number"
+  <GuessNumber />,
   <TelephoneFormatter />,
 ];
 
--- react/src/machine-coding/Guess-the-number/App.jsx
+++ react/src/machine-coding/Guess-the-number/App.jsx
@@ -1,62 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import './Guess.css'
+import React, { useEffect, useState } from "react";
 
-export default function App()  {
-  const[l1,setL1]=useState();
-  var flag=true;
-  useEffect(()=>{
-    setL1(Math.round(100*(Math.random())));
-  },[]);
+import styles from "./guess.module.css";
+
+export default function App() {
+  const [l1, setL1] = useState();
   const [disable, setDisable] = useState(false);
-  const[num,setNum]=useState(1);
-  const[low,setLow]=useState(false);
-  const[high,setHigh]=useState(false);
-  const[hi,setHi]=useState(false);
-   const help=(e)=>{
+  const [num, setNum] = useState(1);
+  const [low, setLow] = useState(false);
+  const [high, setHigh] = useState(false);
+  const [hi, setHi] = useState(false);
+
+  useEffect(() => {
+    setL1(Math.round(100 * Math.random()));
+  }, []);
+
+  const check = (e) => {
     e.preventDefault();
-    console.log(l1);
-     if(num<l1){
-        setLow(true);
-        setHigh(false);
-     }
-     else if(num>l1){
-     setHigh(true);
-     setLow(false);
-     }
-     else {
+
+    if (num < l1) {
+      setLow(true);
+      setHigh(false);
+    } else if (num > l1) {
+      setHigh(true);
+      setLow(false);
+    } else {
       setHi(true);
       setLow(false);
       setHigh(false);
       setDisable(true);
-     }
-  
-   }
-   const Res=()=>{
-    setL1(Math.round(100*(Math.random())));
+    }
+  };
+
+  const restart = () => {
+    setL1(Math.round(100 * Math.random()));
     setHi(false);
     setLow(false);
     setHigh(false);
     setDisable(false);
-   }
-  return (
-    <div className='App'>
-     <input placeholder='Guess-Number'onChange={(e)=>setNum(e.target.value)}/>
-     <div className='widget'>
-     <button onClick={()=>Res()}> Start</button>
-     <button onClick={(e)=>help(e)} disabled={disable}>check</button>
-     </div>
-     <div className='text'>
-      {
-        low && <p>Less than no</p>
-      }
-      {
-        high && <p>higher than no</p>
-      }
-      {
-        flag && hi && <p>your guess is right </p>
-      }
-     </div>
-    </div>
-  )
-}
+  };
 
+  return (
+    <div className={styles.app}>
+      <input
+        placeholder="Guess-Number"
+        onChange={(e) => setNum(e.target.value)}
+      />
+      <div className={styles.widget}>
+        <button onClick={() => restart()}> Start</button>
+        <button onClick={(e) => check(e)} disabled={disable}>
+          check
+        </button>
+      </div>
+      <div className={styles.text}>
+        {low && <p>Your guess is Less than the actual number</p>}
+        {high && <p>Your guess is Higher than the actual number</p>}
+        {hi && <p>Your guess is right</p>}
+      </div>
+    </div>
+  );
+}
