c29f30efdcaefffd6da5c66a61b325b6f7b22307
Rajdeep Chandra <rajdeepc@adobe.com>
2023-07-14 12:15:29 +0530

    added accesibility and typo issue fix

--- react/src/machine-coding/counter/counter.tsx
+++ react/src/machine-coding/counter/counter.tsx
@@ -1,6 +1,6 @@
-import { useRef, useState } from "react";
+import { useRef, useState } from 'react';
 
-import styles from "./counter.module.scss";
+import styles from './counter.module.scss';
 
 function Counter() {
   const [value, setValue] = useState(0);
@@ -26,26 +26,29 @@ function Counter() {
     <main className={styles.main}>
       <h2>{value}</h2>
 
-      <div>
-        <button onClick={decrement}>-</button>
-        <button onClick={increment}>+</button>
-      </div>
+      <section>
+        <button onClick={decrement} aria-label="Decrement">
+          -
+        </button>
+        <button onClick={increment} aria-label="Increment">
+          +
+        </button>
+      </section>
 
-      <div>
-        <label htmlFor="step">Icrement/Decrement by</label>
+      <section>
+        <label htmlFor="step">Increment/Decrement by</label>
         <input
           type="number"
           id="step"
           defaultValue={step.current}
-          onChange={(e) =>
-            setStep((e.target as HTMLInputElement).valueAsNumber)
-          }
+          onChange={(e) => setStep((e.target as HTMLInputElement).valueAsNumber)}
+          title="Step value"
         />
-      </div>
+      </section>
 
-      <div>
+      <section>
         <button onClick={reset}>Reset</button>
-      </div>
+      </section>
     </main>
   );
 }
