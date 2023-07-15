8443fb7d779d88dcaa0955972f6c5ab8971d53ba
rashidtvmr <rashidtvmr@gmail.com>
2023-07-15 12:49:29 +0530

    file and folder structure added

---
+++ react/src/machine-coding/stopwatch/App.jsx
@@ -0,0 +1,13 @@
+import style from './stopwatch.module.css'
+
+// Countdown Component
+const App = () => {
+
+    return (
+        <div className={style['test']}>
+            Test
+        </div>
+    );
+}
+
+export default App;
