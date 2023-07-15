7f53a43546e6a16441534f945525e1cb8637dbf9
rashidtvmr <rashidtvmr@gmail.com>
2023-07-15 12:52:56 +0530

    Stopwatch UI completed

--- react/src/machine-coding/stopwatch/App.jsx
+++ react/src/machine-coding/stopwatch/App.jsx
@@ -5,7 +5,42 @@ const App = () => {
 
     return (
         <div className={style['test']}>
-            Test
+            {/* Watch UI */}
+            <div className={style['watch-container']}>
+                <div className={style['watch']}>
+                    <div>
+                        Stopwatch
+                    </div>
+                    <div className={style['watch-timer']}>
+                        {/* Display the timer string */}
+                        00:00:00
+                    </div>
+                    <div className={style['watch-btn__container']}>
+                        {/* Start button */}
+                        <button
+
+                            className={style['watch-btn']}
+                        >
+                            Start
+                        </button>
+
+                        {/* Stop button */}
+                        <button
+                            className={style['watch-btn']}
+                        >
+                            Stop
+                        </button>
+
+                        {/* Reset button */}
+                        <button
+
+                            className={style['watch-btn']}
+                        >
+                            Reset
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
