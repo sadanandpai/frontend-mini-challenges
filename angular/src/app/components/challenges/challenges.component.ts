5456ba95f802cdf839c9ddf95876a9ba641e6d35
Pankaj Parkar <parkarpankaj3@gmail.com>
2023-07-19 20:41:44 +0800

    Migrated application to standalone components and uses bootstrapApplication method to bootstrap

--- angular/src/app/components/challenges/challenges.component.ts
+++ angular/src/app/components/challenges/challenges.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { challenges } from 'src/app/helpers/challenges';
+import { ChallengeComponent } from '../challenge/challenge.component';
+import { NgFor } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-challenges',
   templateUrl: './challenges.component.html',
   styleUrls: ['./challenges.component.scss'],
+  imports: [ChallengeComponent, NgFor]
 })
 export class ChallengesComponent {
   public challenges = challenges.values();
