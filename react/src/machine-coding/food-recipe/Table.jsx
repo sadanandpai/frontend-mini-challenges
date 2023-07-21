6d1ede0e620cbf88274326c377186fcaa400754d
Kei-K23 <keiksl2301@gmail.com>
2023-07-21 15:52:41 +0630

    Add new challenge: food-recipe

---
+++ react/src/machine-coding/food-recipe/Table.jsx
@@ -0,0 +1,36 @@
+export default function Table({ data }) {
+  const produceIngredientsAndMeasureArray = (ingredients) => {
+    const ingredientsAndMeasureArray = [];
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = ingredients[`strIngredient${i}`];
+      const measure = ingredients[`strMeasure${i}`];
+      if (ingredient && measure) {
+        ingredientsAndMeasureArray.push({ ingredient, measure });
+      }
+    }
+    return ingredientsAndMeasureArray;
+  };
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>Ingredient</th>
+          <th>Measure</th>
+        </tr>
+      </thead>
+      <tbody>
+        {produceIngredientsAndMeasureArray(data).map((item, index) => (
+          <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{item.ingredient}</td>
+            <td>{item.measure}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+Table.propTypes;
