111d37663e2e82c9845392786d51996a161ab6fb
Sadanand Pai <sadanand.pai@coindcx.com>
2023-08-20 11:10:04 +0530

    React: Challenge navbar

---
+++ react/src/components/challenge-navbar/ChallengeNavbar.tsx
@@ -0,0 +1,28 @@
+import styles from './challenge-navbar.module.scss';
+
+interface Props {
+  title?: string;
+}
+
+function ChallengeNavbar({ title }: Props) {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.links}>
+        <a href="#/challenges" className={styles.back}>
+          &lt;
+        </a>
+        <a className={styles.logo} href="/frontend-mini-challenges/">
+          <img src="/frontend-mini-challenges/react/dist/logo.png" alt="logo" />
+        </a>
+      </div>
+
+      <h1>{title}</h1>
+
+      <a href="https://github.com/sadanandpai/frontend-mini-challenges/" target="blank">
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github repo" className={styles.github} />
+      </a>
+    </nav>
+  );
+}
+
+export default ChallengeNavbar;
--- react/src/pages/Challenge.tsx
+++ react/src/pages/Challenge.tsx
@@ -1,12 +1,12 @@
 import Accordion from '@/machine-coding/accordion/App';
 import Autocomplete from '@/machine-coding/autocomplete-offline/autocomplete';
 import AutocompleteOnline from '@/machine-coding/autocomplete-offline/autocompleteOnline';
+import ChallengeNavbar from '@/components/challenge-navbar/ChallengeNavbar';
 import ColumnTable from '@/machine-coding/column-table/ColumnTable';
 import Counter from '@/machine-coding/counter/counter';
 import FileExplorer from '@/machine-coding/file-explorer/App';
 import FoodRecipe from '@/machine-coding/food-recipe/App';
 import GuessNumber from '@/machine-coding/guess-number/App';
-import Header from '@/components/header/header';
 import InfiniteScrolling from '@/machine-coding/infinite-scrolling';
 import InvestmentCalc from '@/machine-coding/investment-calculator/App';
 import LightDarkMode from '@/machine-coding/light-dark-mode/App';
@@ -17,12 +17,13 @@ import ProgressBar from '@/machine-coding/progressbar/App';
 import Stack from '@/machine-coding/stack-implementation/Stack';
 import StarRating from '@/machine-coding/star-rating/App';
 import Stopwatch from '@/machine-coding/stopwatch/App';
+import TableColorizer from '@/machine-coding/table-colorizer/TableColorizer';
 import TelephoneFormatter from '@/machine-coding/telephone-formatter';
 import TicTacToe from '@/machine-coding/tic-tac-toe/App';
 import Toast from '@/machine-coding/toast-popup/toast';
 import TodoList from '@/machine-coding/todo-list/todo';
-import TableColorizer from '@/machine-coding/table-colorizer/TableColorizer';
 import TwentyfiveFiveClock from '@/machine-coding/25-5-clock';
+import { challenges } from '@/helpers/challenges';
 import { useParams } from 'react-router-dom';
 
 const reactChallenges = {
@@ -57,10 +58,10 @@ function Challenge() {
   const id = params?.id ?? '';
 
   return (
-    <div className="container">
-      <Header id={id} />
-      {reactChallenges[id]}
-    </div>
+    <>
+      <ChallengeNavbar title={challenges.get(id)?.title} />
+      <div className="container">{reactChallenges[id]}</div>
+    </>
   );
 }
 
