4d99248e2fb18123c59f6948144a1e565e581adb
Sadanand Pai <sadanand.pai@coindcx.com>
2023-08-31 10:17:57 +0530

    Native: Color Gen: Trivial changes

--- native/machine-coding/color-generator/script.js
+++ native/machine-coding/color-generator/script.js
@@ -1,28 +1,28 @@
 function generateGradient() {
-    let colors = [];
-    for (let i = 0; i < 2; i++) {
-        colors.push(getRandomColor());
-    }
-    let gradient = "linear-gradient(to right, " + colors[0] + ", " + colors[1] + ")";
-    document.querySelector('.gradient').style.background = gradient;
+  let colors = [];
+  for (let i = 0; i < 2; i++) {
+    colors.push(getRandomColor());
+  }
+  let gradient = 'linear-gradient(to right, ' + colors[0] + ', ' + colors[1] + ')';
+  document.querySelector('.gradient').style.background = gradient;
 }
 
 function getRandomColor() {
-    let letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 function copyGradient() {
-    let gradient = document.querySelector('.gradient').style.background;
-    let tempInput = document.createElement("input");
-    tempInput.value = gradient;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
-    alert("Gradient copied to clipboard: " + gradient);
+  let gradient = document.querySelector('.gradient').style.background;
+  let tempInput = document.createElement('input');
+  tempInput.value = gradient;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand('copy');
+  document.body.removeChild(tempInput);
+  alert('Gradient copied to clipboard: ' + gradient);
 }
