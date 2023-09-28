98719e94267fcb02399a98ce54c0ab33f8b69f37
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-09-28 12:26:23 -0400

    Project done. I'll separate it into components

--- react/src/machine-coding/pagination/App.jsx
+++ react/src/machine-coding/pagination/App.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from 'react';
+import styles from './App.module.css';
+import { FOODS } from './data/data.js';
+import { useState } from 'react';
+
+const Table = ({ data, currentPage, totalPages, onPageChange }) => {
+  return (
+    <div className={styles.container_for_table}>
+        
+      <div className={styles.pagination}>
+        <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+        <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
+
+      <table className={styles.table}>
+        <thead className={styles.thead}>
+          <tr>
+            <th>#</th>
+            <th>Food</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody className={styles.tbody}>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.price}$</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 const App = () => {
-  return (
-    <div>Pagination</div>
-  )
-}
+  const ITEMS_PER_PAGE = 10;
+  const [currentPage, setCurrentPage] = useState(1);
 
-export default App
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  const paginatedData = FOODS.slice(startIndex, endIndex);
+
+  return (
+    <div className={styles.pagination_container}>
+      <h1 className={styles.title}>Food Pagination Example</h1>
+      <Table
+        data={paginatedData}
+        currentPage={currentPage}
+        totalPages={Math.ceil(FOODS.length / ITEMS_PER_PAGE)}
+        onPageChange={handlePageChange}
+      />
+    </div>
+  );
+};
+
+export default App;
