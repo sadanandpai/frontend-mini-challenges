8277f2b257fb904765d260cc349c23fc4a457e51
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-09-28 12:35:39 -0400

    Pagination project ready and divided into components

--- react/src/machine-coding/pagination/App.jsx
+++ react/src/machine-coding/pagination/App.jsx
@@ -2,44 +2,7 @@ import React from 'react';
 import styles from './App.module.css';
 import { FOODS } from './data/data.js';
 import { useState } from 'react';
-
-const Table = ({ data, currentPage, totalPages, onPageChange }) => {
-  return (
-    <div className={styles.container_for_table}>
-        
-      <div className={styles.pagination}>
-        <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>
-          Previous
-        </button>
-        <span>
-          Page {currentPage} of {totalPages}
-        </span>
-        <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>
-          Next
-        </button>
-      </div>
-
-      <table className={styles.table}>
-        <thead className={styles.thead}>
-          <tr>
-            <th>#</th>
-            <th>Food</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody className={styles.tbody}>
-          {data.map((item, index) => (
-            <tr key={index}>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>{item.price}$</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+import { Table } from './components/Table/Table';
 
 const App = () => {
   const ITEMS_PER_PAGE = 10;
