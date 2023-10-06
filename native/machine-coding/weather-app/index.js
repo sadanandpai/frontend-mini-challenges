939ad56da2d6bc9e31fa9b2380f946f47390414a
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2023-10-06 22:13:12 +0530

    Update index.js

--- native/machine-coding/weather-app/index.js
+++ native/machine-coding/weather-app/index.js
@@ -20,21 +20,7 @@ async function checkWeather(city) {
     document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
     document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h';
 
-    if(data.weather[0].main == "Clouds"){
-        weatherIcon.src = "images/clouds.png"
-    }
-    else if(data.weather[0].main == "Clear"){
-        weatherIcon.src = "images/clear.png"
-    }
-    else if(data.weather[0].main == "Rain"){
-        weatherIcon.src = "images/rain.png"
-    }
-    else if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src = "images/drizzle.png"
-    }
-    else if(data.weather[0].main == "Mist"){
-        weatherIcon.src = "images/mist.png"
-    }
+    weatherIcon.src = `images/${data.weather[0]?.main?.toLowerCase()}.png`;
 
     document.querySelector('.weather').style.display = 'block';
     document.querySelector('.error').style.display = 'none';
