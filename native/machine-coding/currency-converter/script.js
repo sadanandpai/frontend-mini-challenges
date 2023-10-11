3938c60807e533e271599b25697a3df66dfd5bff
pranav514 <pranavsalunkhe327@gmail.com>
2023-10-11 17:37:18 +0530

    issue resolved

--- native/machine-coding/currency-converter/script.js
+++ native/machine-coding/currency-converter/script.js
@@ -6,6 +6,45 @@ const currencyRates = {
     // Add more currency rates here
 };
 
+document.getElementById("addCurrencyButton").addEventListener("click", function() {
+    const customCurrencyCode = document.getElementById("customCurrency").value.toUpperCase();
+    if (customCurrencyCode && !currencyRates[customCurrencyCode]) {
+        const exchangeRate = parseFloat(prompt(`Enter exchange rate for 1 USD to ${customCurrencyCode}`));
+        if (!isNaN(exchangeRate)) {
+            currencyRates[customCurrencyCode] = exchangeRate;
+            updateCurrencyOptions();
+        } else {
+            alert("Invalid exchange rate. Please enter a valid number.");
+        }
+    } else if (currencyRates[customCurrencyCode]) {
+        alert(`Currency ${customCurrencyCode} already exists.`);
+    } else {
+        alert("Invalid currency code. Please enter a valid code (e.g., CAD).");
+    }
+});
+
+function updateCurrencyOptions() {
+    const fromCurrencySelect = document.getElementById("fromCurrency");
+    const toCurrencySelect = document.getElementById("toCurrency");
+
+    // Clear existing options
+    fromCurrencySelect.innerHTML = "";
+    toCurrencySelect.innerHTML = "";
+
+    for (const currency in currencyRates) {
+        const option = document.createElement("option");
+        option.value = currency;
+        option.innerText = currency;
+        fromCurrencySelect.appendChild(option);
+
+        const toOption = option.cloneNode(true);
+        toCurrencySelect.appendChild(toOption);
+    }
+    toCurrencySelect.innerHTML += '<option value="custom">Custom</option>';
+}
+
+document.getElementById("convertButton").addEventListener("click", convertCurrency);
+
 function convertCurrency() {
     const amount = parseFloat(document.getElementById("amount").value);
     const fromCurrency = document.getElementById("fromCurrency").value;
@@ -20,3 +59,6 @@ function convertCurrency() {
     const convertedAmount = (amount / currencyRates[fromCurrency]) * currencyRates[toCurrency];
     resultElement.value = convertedAmount.toFixed(2);
 }
+
+// Initialize currency options
+updateCurrencyOptions();
