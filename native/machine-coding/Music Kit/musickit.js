64c82b310c29ecba7655814eff58a5f4e4a7d981
Shivam Kumar <127589548+shivam200446@users.noreply.github.com>
2023-10-11 21:48:42 +0530

    Update musickit.js

--- native/machine-coding/Music Kit/musickit.js
+++ native/machine-coding/Music Kit/musickit.js
@@ -1,10 +1,10 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+const numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
+for (const i = 0; i < numberOfDrumButtons; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-    var buttonInnerHTML = this.innerHTML;
+    const buttonInnerHTML = this.innerHTML;
 
     makeSound(buttonInnerHTML);
 
@@ -27,37 +27,37 @@ function makeSound(key) {
 
   switch (key) {
     case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
+      const tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
     case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
+       const tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
     case "s":
-      var tom3 = new Audio('sounds/tom-3.mp3');
+       const tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
 
     case "d":
-      var tom4 = new Audio('sounds/tom-4.mp3');
+       const tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
 
     case "j":
-      var snare = new Audio('sounds/snare.mp3');
+       const snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
 
     case "k":
-      var crash = new Audio('sounds/crash.mp3');
+       const crash = new Audio('sounds/crash.mp3');
       crash.play();
       break;
 
     case "l":
-      var kick = new Audio('sounds/kick-bass.mp3');
+       const kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
       break;
 
@@ -70,12 +70,8 @@ function makeSound(key) {
 
 function buttonAnimation(currentKey) {
 
-  var activeButton = document.querySelector("." + currentKey);
+   const activeButton = document.querySelector("." + currentKey);
 
   activeButton.classList.add("pressed");
-
-  setTimeout(function() {
-    activeButton.classList.remove("pressed");
-  }, 170);
 
 }
