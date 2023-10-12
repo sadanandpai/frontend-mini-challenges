6cfb3bb080bd27119184a8249b53fc128f204d75
Sadanand Pai <sadanand.pai@coindcx.com>
2023-10-12 22:54:16 +0530

    Contributors list update

--- native/helpers/contributors.js
+++ native/helpers/contributors.js
@@ -1,17 +1,54 @@
 export const contributors = new Map([
-  ['sadanandpai', { name: 'Sadanand Pai', pic: 'https://avatars.githubusercontent.com/u/12962887' }],
-  ['noorulaink00', { name: 'Noor Ul Ain Khan', pic: 'https://avatars.githubusercontent.com/u/65324193' }],
-  ['sahankatta', { name: 'Sahan Katta', pic: 'https://avatars.githubusercontent.com/u/65811236?v=4' }],
+  [
+    'sadanandpai',
+    { name: 'Sadanand Pai', pic: 'https://avatars.githubusercontent.com/u/12962887' },
+  ],
+  [
+    'noorulaink00',
+    { name: 'Noor Ul Ain Khan', pic: 'https://avatars.githubusercontent.com/u/65324193' },
+  ],
+  ['sahankatta', { name: 'Sahan Katta', pic: 'https://avatars.githubusercontent.com/u/65811236' }],
   ['rashidtvmr', { name: 'Rashid', pic: 'https://avatars.githubusercontent.com/u/58169715' }],
-  ['gauravsinhaweb', { name: 'Gaurav Sinha', pic: 'https://avatars.githubusercontent.com/u/75125943' }],
+  [
+    'gauravsinhaweb',
+    { name: 'Gaurav Sinha', pic: 'https://avatars.githubusercontent.com/u/75125943' },
+  ],
   ['Kei-K23', { name: 'Kei-K', pic: 'https://avatars.githubusercontent.com/u/134714087' }],
-  ['caesar003', { name: 'Khaisar Muksid', pic: 'https://avatars.githubusercontent.com/u/37962465' }],
-  ['christian-lee1398', { name: 'Christian Lee', pic: 'https://avatars.githubusercontent.com/u/83561483' }],
-  ['Sukomal07', { name: 'Sukomal Dutta', pic: 'https://avatars.githubusercontent.com/u/100500478' }],
+  [
+    'caesar003',
+    { name: 'Khaisar Muksid', pic: 'https://avatars.githubusercontent.com/u/37962465' },
+  ],
+  [
+    'christian-lee1398',
+    { name: 'Christian Lee', pic: 'https://avatars.githubusercontent.com/u/83561483' },
+  ],
+  [
+    'Sukomal07',
+    { name: 'Sukomal Dutta', pic: 'https://avatars.githubusercontent.com/u/100500478' },
+  ],
   ['5h0ov', { name: 'Shuvadipta Das', pic: 'https://avatars.githubusercontent.com/u/83227649' }],
-  ['shruti-sen2004', { name: 'Shruti Sen', pic: 'https://avatars.githubusercontent.com/u/115914670' }],
-  ['Kushal347', { name: 'Kushal Agrawal', pic: 'https://avatars.githubusercontent.com/u/115914670' }],
-  ['AbhineshJha', { name: 'ABHINESH KUMAR JHA', pic: 'https://avatars.githubusercontent.com/u/142514166' }],
-  ['hritik', { name: 'Hritik Bhattacharya', pic: 'https://avatars.githubusercontent.com/u/44370586?v=4' }],
-  ['viditagrawal56', { name: 'Vidit Agrawal', pic: 'https://avatars.githubusercontent.com/u/52532308?v=4' }],
+  [
+    'shruti-sen2004',
+    { name: 'Shruti Sen', pic: 'https://avatars.githubusercontent.com/u/115914670' },
+  ],
+  [
+    'Kushal347',
+    { name: 'Kushal Agrawal', pic: 'https://avatars.githubusercontent.com/u/115914670' },
+  ],
+  [
+    'AbhineshJha',
+    { name: 'ABHINESH KUMAR JHA', pic: 'https://avatars.githubusercontent.com/u/142514166' },
+  ],
+  [
+    'hritik',
+    { name: 'Hritik Bhattacharya', pic: 'https://avatars.githubusercontent.com/u/44370586' },
+  ],
+  [
+    'viditagrawal56',
+    { name: 'Vidit Agrawal', pic: 'https://avatars.githubusercontent.com/u/52532308' },
+  ],
+  [
+    'VishakhaSainani',
+    { name: 'Vishakha Sainani', pic: 'https://avatars.githubusercontent.com/u/113436770' },
+  ],
 ]);
