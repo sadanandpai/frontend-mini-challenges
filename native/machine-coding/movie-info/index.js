f11deafc78bfc22cf123e26aec7ae6f1dc3a43c1
AckermanLevi1 <aryan.bhargava.mec21@itbhu.ac.in>
2023-10-14 02:33:30 +0530

    Changes Added

--- native/machine-coding/movie-info/index.js
+++ native/machine-coding/movie-info/index.js
@@ -1,3 +1,5 @@
+// main.js
+
 const movieTitle = document.getElementById('movie-title');
 const movieGenre = document.getElementById('movie-genre');
 const movieCast = document.getElementById('movie-cast');
@@ -5,40 +7,34 @@ const movieRating = document.getElementById('movie-rating');
 const movieReleased = document.getElementById('movie-released');
 const movieDirector = document.getElementById('movie-director');
 const moviePoster = document.getElementById('movie-poster');
+const apiKey = 'adff2bf8';
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Run this code when the page is fully loaded and ready
-    const movieInfoDiv = document.querySelector('.movie-info');
-    movieInfoDiv.style.display = 'none';
+const movieInfoDiv = document.querySelector('.movie-info');
+movieInfoDiv.style.display = 'none';
 
-    const searchButton = document.getElementById('search-button');
-    const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
 
-    searchButton.addEventListener('click', () => {
-        // Fetch movie data from an API or your database and update the DOM elements accordingly.
-        // For example, you can use the OMDB API for this purpose.
-        const apiKey = 'adff2bf8';
-        const query = searchInput.value;
+searchButton.addEventListener('click', () => {
+    const query = searchInput.value;
 
-        // Make an API request to get movie data
-        fetch(`https://www.omdbapi.com/?t=${query}&apikey=${apiKey}`)
-            .then(response => response.json())
-            .then(data => {
-                
+    function updateMovieInfo(data) {
+        movieTitle.textContent = data.Title;
+        movieGenre.textContent = data.Genre;
+        movieCast.textContent = data.Actors;
+        movieRating.textContent = data.imdbRating;
+        movieReleased.textContent = data.Released;
+        movieDirector.textContent = data.Director;
+        moviePoster.src = data.Poster;
+        movieInfoDiv.style.display = 'block';
+    }
 
-                movieTitle.textContent = data.Title;
-                movieGenre.textContent = data.Genre;
-                movieCast.textContent = data.Actors;
-                movieRating.textContent = data.imdbRating;
-                movieReleased.textContent = data.Released;
-                movieDirector.textContent = data.Director;
-                moviePoster.src = data.Poster;
-
-                // Show the "movie-info" div when data is available
-                movieInfoDiv.style.display = 'block';
-            })
-            .catch(error => {
-                console.error('Error fetching data: ', error);
-            });
-    });
+    fetch(`https://www.omdbapi.com/?t=${query}&apikey=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            updateMovieInfo(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data: ', error);
+        });
 });
