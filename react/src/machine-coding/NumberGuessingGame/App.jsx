06e82d76a313cd5de15f36d4d9ec56506ec78ce3
Ohm Ramwala <60615314+OhmRamwala@users.noreply.github.com>
2023-10-20 19:22:25 +0530

    Create App.jsx

---
+++ react/src/machine-coding/NumberGuessingGame/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NumberGuessingGameComponent from './NumberGuessingGameComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <NumberGuessingGameComponent />
+    </div>
+  );
+}
+
+export default App;
