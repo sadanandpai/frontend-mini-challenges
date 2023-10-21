0fb1925e5bf6b16e2a5318299949f0ca6227f424
Vivek-GuptaXCode <vivekgupta040202@gmail.com>
2023-10-21 11:47:32 +0530

    [Feature Update] Added more questions and select 10 questions randomly.

--- native/machine-coding/quiz-app-with-timer/index.js
+++ native/machine-coding/quiz-app-with-timer/index.js
@@ -9,7 +9,7 @@ const restart = document.getElementById("restart");
 const userScore = document.getElementById("user-score");
 const startScreen = document.querySelector(".start-screen");
 const startButton = document.getElementById("start-button");
-let questionCount = 0; // Initialize questionCount
+let questionCount = 0;
 let scoreCount = 0;
 let count = 11;
 let countdown;
@@ -17,44 +17,43 @@ let countdown;
 // Quiz questions array
 let quizArray;
 
-// Fetch the questions from the JSON file
+// Function to shuffle and select random questions
+function selectRandomQuestions(questions, count) {
+  const shuffledQuestions = questions.slice().sort(() => Math.random() - 0.5);
+  return shuffledQuestions.slice(0, count);
+}
+
+// Fetch the questions from the JSON file and initialize the quiz
 fetch("questions.json")
     .then((response) => response.json())
     .then((data) => {
         quizArray = data;
-        quizCreator();
-        initial(); // Initialize the quiz
+        initial(); // Initialize the quiz with random questions
     })
     .catch((error) => {
         console.error("Error loading questions:", error);
     });
 
-//Restart Quiz
+// Restart Quiz
 restart.addEventListener("click", () => {
   initial();
   displayContainer.classList.remove("hide");
   scoreContainer.classList.add("hide");
 });
 
-//Next Button
+// Next Button
 nextBtn.addEventListener(
   "click",
   (displayNext = () => {
-    //increment questionCount
     questionCount += 1;
-    //if last question
-    if (questionCount == quizArray.length) {
-      //hide question container and display score
+    if (questionCount == selectedQuestions.length) {
       displayContainer.classList.add("hide");
       scoreContainer.classList.remove("hide");
-      //user score
       userScore.innerHTML =
-        "Your score is " + scoreCount + " out of " + questionCount;
+        "Your score is " + scoreCount + " out of " + selectedQuestions.length;
     } else {
-      //display questionCount
       countOfQuestion.innerHTML =
-        questionCount + 1 + " of " + quizArray.length + " Question";
-      //display quiz
+        questionCount + 1 + " of " + selectedQuestions.length + " Question";
       quizDisplay(questionCount);
       count = 11;
       clearInterval(countdown);
@@ -63,7 +62,7 @@ nextBtn.addEventListener(
   })
 );
 
-//Timer
+// Timer
 const timerDisplay = () => {
   countdown = setInterval(() => {
     count--;
@@ -75,42 +74,27 @@ const timerDisplay = () => {
   }, 1000);
 };
 
-//Display quiz
+// Display quiz
 const quizDisplay = (questionCount) => {
   const quizCards = document.querySelectorAll(".container-mid");
-  //Hide other cards
   quizCards.forEach((card) => {
     card.classList.add("hide");
   });
-  //display current question card
   quizCards[questionCount].classList.remove("hide");
 };
 
-//Quiz Creation
+// Quiz Creation
 function quizCreator() {
-  // Check if quizArray is loaded
-  if (!quizArray) {
-    console.error("Quiz questions not loaded yet.");
-    return;
-  }
-
-  //randomly sort questions
   quizArray.sort(() => Math.random() - 0.5);
-  //generate quiz
-  for (const i of quizArray) {
-    //randomly sort options
+  for (const i of selectedQuestions) {
     i.options.sort(() => Math.random() - 0.5);
-    //quiz card creation
     const div = document.createElement("div");
     div.classList.add("container-mid", "hide");
-    //question number
-    countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
-    //question
+    countOfQuestion.innerHTML = 1 + " of " + selectedQuestions.length + " Question";
     const question_DIV = document.createElement("p");
     question_DIV.classList.add("question");
     question_DIV.innerHTML = i.question;
     div.appendChild(question_DIV);
-    //options
     div.innerHTML += `
     <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
      <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
@@ -121,57 +105,56 @@ function quizCreator() {
   }
 }
 
-//Checker Function to check if option is correct or not
+// Checker Function
 function checker(userOption) {
   const userSolution = userOption.innerText;
   const question =
     document.getElementsByClassName("container-mid")[questionCount];
   const options = question.querySelectorAll(".option-div");
 
-  //if user clicked answer == correct option stored in object
-  if (userSolution === quizArray[questionCount].correct) {
+  if (userSolution === selectedQuestions[questionCount].correct) {
     userOption.classList.add("correct");
     scoreCount++;
   } else {
     userOption.classList.add("incorrect");
-    //For marking the correct option
     options.forEach((element) => {
-      if (element.innerText == quizArray[questionCount].correct) {
+      if (element.innerText == selectedQuestions[questionCount].correct) {
         element.classList.add("correct");
       }
     });
   }
 
-  //clear interval(stop timer)
   clearInterval(countdown);
-  //disable all options
   options.forEach((element) => {
     element.disabled = true;
   });
 }
 
-//initial setup
+// Initial setup
 function initial() {
   quizContainer.innerHTML = "";
   questionCount = 0;
   scoreCount = 0;
   count = 11;
   clearInterval(countdown);
+
+  // Select 10 random questions from the pool of questions
+  selectedQuestions = selectRandomQuestions(quizArray, 10);
+
   timerDisplay();
   quizCreator();
   quizDisplay(questionCount);
 }
 
-//when user click on start button
+// Start button click event
 startButton.addEventListener("click", () => {
   startScreen.classList.add("hide");
   displayContainer.classList.remove("hide");
   initial();
 });
 
-//hide quiz and display start screen
+// Hide the quiz and display the start screen on page load
 window.onload = () => {
   startScreen.classList.remove("hide");
   displayContainer.classList.add("hide");
 };
-
