cf7dda357def46f84ebc4616c31b58d6b4180047
Bratajit Das <106532791+Bratajit-03@users.noreply.github.com>
2023-10-22 19:27:58 +0530

    challenges.js

--- native/helpers/challenges.js
+++ native/helpers/challenges.js
@@ -32,14 +32,6 @@ export const challenges = [
     tags: [],
   },
   {
-    title: 'File Uploader',
-    link: 'file-uploader',
-    difficulty: 'easy',
-    developer: 'avtech',
-    tags: [],
-    isNew: true,
-  },
-  {
     title: 'Telephone formatter',
     link: 'telephone-formatter',
     difficulty: 'easy',
