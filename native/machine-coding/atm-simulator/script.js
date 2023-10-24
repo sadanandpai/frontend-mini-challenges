86ea08e2a6d92f9fd8525305e9ca75df3281df48
Sadanand Pai <sadanand.pai@coindcx.com>
2023-10-24 17:03:59 +0530

    ATM sim refactor

--- native/machine-coding/atm-simulator/script.js
+++ native/machine-coding/atm-simulator/script.js
@@ -12,63 +12,63 @@ depositButton.addEventListener('click', deposit);
 withdrawButton.addEventListener('click', withdraw);
 logoutButton.addEventListener('click', logout);
 
-const correctPin = "1234";
+const correctPin = '1234';
 let isAuthenticated = false;
 let balance = 1000;
 
 function authenticate() {
-    const pin = document.getElementById("pin").value;
-    if (pin === correctPin) {
-        isAuthenticated = true;
-        displayMessage("Login successful.");
-    } else {
-        isAuthenticated = false;
-        displayMessage("Invalid PIN. Please try again.");
-    }
+  const pin = document.getElementById('pin').value;
+  if (pin === correctPin) {
+    isAuthenticated = true;
+    displayMessage('Login successful.');
+  } else {
+    isAuthenticated = false;
+    displayMessage('Invalid PIN. Please try again.');
+  }
 }
 
 function checkBalance() {
-    if (!isAuthenticated) {
-        displayMessage("Please login first.");
-        return;
-    }
-    displayMessage(`Your balance is $${balance}`);
+  if (!isAuthenticated) {
+    displayMessage('Please login first.');
+    return;
+  }
+  displayMessage(`Your balance is $${balance}`);
 }
 
 function deposit() {
-    if (!isAuthenticated) {
-        displayMessage("Please login first.");
-        return;
-    }
-    const amount = parseFloat(prompt("Enter the amount to deposit:"));
-    if (!isNaN(amount) && amount > 0) {
-        balance += amount;
-        displayMessage(`Deposited $${amount}. Your new balance is $${balance}`);
-    } else {
-        displayMessage("Invalid amount. Please try again.");
-    }
+  if (!isAuthenticated) {
+    displayMessage('Please login first.');
+    return;
+  }
+  const amount = parseFloat(prompt('Enter the amount to deposit:'));
+  if (!isNaN(amount) && amount > 0) {
+    balance += amount;
+    displayMessage(`Deposited $${amount}. Your new balance is $${balance}`);
+  } else {
+    displayMessage('Invalid amount. Please try again.');
+  }
 }
 
 function withdraw() {
-    if (!isAuthenticated) {
-        displayMessage("Please login first.");
-        return;
-    }
-    const amount = parseFloat(prompt("Enter the amount to withdraw:"));
-    if (!isNaN(amount) && amount > 0 && amount <= balance) {
-        balance -= amount;
-        displayMessage(`Withdrawn $${amount}. Your new balance is $${balance}`);
-    } else {
-        displayMessage("Invalid amount or insufficient balance. Please try again.");
-    }
+  if (!isAuthenticated) {
+    displayMessage('Please login first.');
+    return;
+  }
+  const amount = parseFloat(prompt('Enter the amount to withdraw:'));
+  if (!isNaN(amount) && amount > 0 && amount <= balance) {
+    balance -= amount;
+    displayMessage(`Withdrawn $${amount}. Your new balance is $${balance}`);
+  } else {
+    displayMessage('Invalid amount or insufficient balance. Please try again.');
+  }
 }
 
 function logout() {
-    isAuthenticated = false;
-    displayMessage("Logged out successfully.");
+  isAuthenticated = false;
+  displayMessage('Logged out successfully.');
 }
 
 function displayMessage(message) {
-    const outputDiv = document.getElementById("output");
-    outputDiv.textContent = message;
+  const outputDiv = document.getElementById('output');
+  outputDiv.textContent = message;
 }
