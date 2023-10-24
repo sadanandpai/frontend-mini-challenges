4104d3cae1ea42fda72ecd8f65b19030d0489749
arpansaha13 <arpansaha1300@gmail.com>
2023-10-24 15:32:03 +0530

    Revert "Delete react/src/machine-coding/todo-list/todo.jsx"
    This reverts commit af0097990d6ab83a676c06ffb9250e25ab32b11a.

---
+++ react/src/machine-coding/todo-list/todo.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import List from "./list";
+import styles from "./todo.module.scss";
+
+const Todo = () => {
+  const [value, setValue] = useState("");
+  const [items, setItems] = useState([]);
+  const [editInfo, setEditInfo] = useState(null);
+
+  const addItem = (value) => {
+    setItems((prevItems) => [
+      ...prevItems,
+      { value, id: new Date().getTime(), isDone: false }
+    ]);
+  };
+
+  const updateItem = (newValue) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === editInfo.id ? { ...item, value: newValue } : item
+      )
+    );
+    setEditInfo(null);
+  };
+
+  const handleCompleteClick = (id) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, isDone: !item.isDone } : item
+      )
+    );
+  };
+
+  const handleEditClick = ({ id, value }) => {
+    setValue(value);
+    setEditInfo({ id, value });
+  };
+
+  const handleDeleteClick = (id) => {
+    if (editInfo?.id === id) {
+      setValue("");
+      setEditInfo(null);
+    }
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (editInfo) {
+      updateItem(value);
+    } else {
+      addItem(value);
+    }
+    setValue("");
+  };
+
+  const cancelHandler = () => {
+    setValue("");
+    setEditInfo(null);
+  };
+
+  return (
+    <div className={styles.App}>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          value={value}
+          placeholder="Enter your todo"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type="submit" disabled={!value}>
+          {editInfo ? "Update" : "Submit"}
+        </button>
+        <button
+          type="reset"
+          onClick={cancelHandler}
+          disabled={!(value || editInfo)}
+        >
+          Cancel
+        </button>
+      </form>
+
+      <List
+        items={items}
+        handleEditClick={handleEditClick}
+        handleDeleteClick={handleDeleteClick}
+        handleCompleteClick={handleCompleteClick}
+      />
+    </div>
+  );
+};
+
+export default Todo;
