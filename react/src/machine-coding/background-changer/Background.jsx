cdec8a2fe7a8f8b5f34eca93a40e14a25acd6290
Adidem23 <adityasuryawanshi5451@gmail.com>
2023-10-25 23:19:04 +0530

    Added ButtonBase class and changed Async fucntion

--- react/src/machine-coding/background-changer/Background.jsx
+++ react/src/machine-coding/background-changer/Background.jsx
@@ -3,49 +3,17 @@ import styles from './background.module.css';
 
 const Background = () => {
 
-  const ChangeBg = async () => {
-
-    // <-----------This is request with Axios----------->
-
-    // const options = {
-    //   method: 'GET',
-    //   url: 'https://colorful3.p.rapidapi.com/randomColor/2',
-    //   headers: {
-    //     'X-RapidAPI-Key': '4638530cfemshb5f9bea28be8a72p1d9bb8jsnef9a6f833608',
-    //     'X-RapidAPI-Host': 'colorful3.p.rapidapi.com'
-    //   }
-    // };
-
-    // try {
-    //   const response = await axios.request(options);
-    //   console.log(response.data[0].hex);
-    //   document.body.style.backgroundColor=`#${response.data[0].hex}`;
-    // } catch (error) {
-    //   console.error(error);
-    // }
-
-    // <-----------request with Axios Ends Here ----------->
-
-
-    const url = 'https://colorful3.p.rapidapi.com/randomNamedColor/2';
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': '4638530cfemshb5f9bea28be8a72p1d9bb8jsnef9a6f833608',
-        'X-RapidAPI-Host': 'colorful3.p.rapidapi.com'
-      }
-    };
-
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      console.log(result[0].hex);
-      document.body.style.backgroundColor = `#${result[0].hex}`;
-    } catch (error) {
-      console.error(error);
+  const ChangeBg = () => {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
     }
 
+    document.body.style.backgroundColor = color;
   }
+
+
 
   return (
     <>
@@ -53,7 +21,7 @@ const Background = () => {
         <button type="button" className={styles.button} onClick={ChangeBg}>
           <div className={styles.buttontop}>Change Colour!!😎</div>
           <div className={styles.buttonbottom}></div>
-          <div className={styles.buttonbase}></div>
+          <div className={styles.buttonBase}></div>
         </button>
       </div>
     </>
