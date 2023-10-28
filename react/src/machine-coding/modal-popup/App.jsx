70aac9e3cdf667f8b22e381dc4740f32af44874b
arpansaha13 <arpansaha1300@gmail.com>
2023-10-28 13:51:14 +0530

    bring modal data inside App.jsx

--- react/src/machine-coding/modal-popup/App.jsx
+++ react/src/machine-coding/modal-popup/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import { createPortal } from 'react-dom'
 import styles from './Modal.module.css'
 import ModalPopup from './ModalPopup'
-import modaldata from './modaldata'
 
 const App = () => {
   const [showModal, setShowModal] = useState(false)
+
+  const modaldata = {
+    title: "Modal Heading",
+    content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, voluptates. Nobis delectus magni quaerat et deserunt fuga temporibus doloribus vero quidem repellat suscipit nulla tempora eveniet, repellendus deleniti placeat quibusdam."
+  }
 
   return (
     <>
--- react/src/machine-coding/modal-popup/modaldata.ts
+++
@@ -1,6 +0,0 @@
-const modaldata = {
-  title: "Modal Heading",
-  content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, voluptates. Nobis delectus magni quaerat et deserunt fuga temporibus doloribus vero quidem repellat suscipit nulla tempora eveniet, repellendus deleniti placeat quibusdam."
-}
-
-export default modaldata
