6ded4620724eb94d48827211e9a4e9c7943338c3
arpansaha13 <arpansaha1300@gmail.com>
2023-10-28 13:26:56 +0530

    rename modaldata.jsx to modaldata.ts

---
+++ react/src/machine-coding/modal-popup/modaldata.ts
@@ -0,0 +1,6 @@
+const modaldata ={
+  title: "Modal Heading",
+  content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, voluptates. Nobis delectus magni quaerat et deserunt fuga temporibus doloribus vero quidem repellat suscipit nulla tempora eveniet, repellendus deleniti placeat quibusdam."
+}
+
+export default modaldata
