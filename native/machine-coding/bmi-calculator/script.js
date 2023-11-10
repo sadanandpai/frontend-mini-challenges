5ac1eaf9c5b9eda8caf9e419dddca2c96cedb783
Papi <111683116+itsayopapi@users.noreply.github.com>
2023-11-10 08:19:48 +0200

    Update script.js

--- native/machine-coding/bmi-calculator/script.js
+++ native/machine-coding/bmi-calculator/script.js
@@ -1,15 +1,40 @@
-function calculateBMI() {
-    var height = parseFloat(document.getElementById("height").value);
-    var weight = parseFloat(document.getElementById("weight").value);
-  
-    if (isNaN(height) || isNaN(weight)) {
-      alert("Please enter valid height and weight.");
-      return;
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('bmiForm');
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Get the input values
+    const height = parseFloat(document.getElementById('height').value);
+    const weight = parseFloat(document.getElementById('weight').value);
+
+    // Check if height and weight are valid numbers
+    if (!isNaN(height) && !isNaN(weight)) {
+      // Calculate BMI
+      const bmi = calculateBMI(height, weight);
+
+      // Display BMI result
+      displayBMI(bmi);
+    } else {
+      alert('Please enter valid height and weight values.');
     }
-  
-    var bmi = weight / (height * height);
-    var result = "Your BMI is: " + bmi.toFixed(2);
-  
-    document.getElementById("result").innerHTML = result;
+  });
+
+  // Function to calculate BMI
+  function calculateBMI(height, weight) {
+    // BMI Formula: weight (kg) / (height (m) * height (m))
+    const heightInMeters = height / 100; // Convert height to meters
+    return (weight / (heightInMeters * heightInMeters)).toFixed(2);
   }
-  
+
+  // Function to display BMI
+  function displayBMI(bmi) {
+    const resultContainer = document.getElementById('result');
+    const bmiValueElement = document.getElementById('bmiValue');
+
+    // Display BMI value
+    bmiValueElement.textContent = bmi;
+
+    // Show the result container
+    resultContainer.style.display = 'block';
+  }
+});
