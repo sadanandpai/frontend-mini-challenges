7f2475ee07e94cb10bf244edcf7b6dc1eb9c50a0
proghax333 <skillzworldtech@gmail.com>
2023-11-14 20:00:37 +0530

    fix(React/Table Colorizer): show error message below input

--- react/src/machine-coding/table-colorizer/TableColorizer.jsx
+++ react/src/machine-coding/table-colorizer/TableColorizer.jsx
@@ -8,24 +8,42 @@ const numberList = Array.from(new Array(length), (_, i) => i);
 const TableColorizer = () => {
   const [colorArray, setColorArray] = useState([]);
   const colRef = useRef();
+  const [inputError, setInputError] = useState(null);
 
   const onColorMe = (e) => {
     e.preventDefault();
-    if (parseInt(colRef.current.value) > 9 || parseInt(colRef.current.value) < 1) {
-      alert('enter Valid Number');
+
+    const cellNumber = parseInt(colRef.current.value);
+
+    if (isNaN(cellNumber) || cellNumber > 9 || cellNumber < 1) {
+      setInputError('Please enter a valid number between 1 to 9');
+      return;
     }
-    if (colorArray.indexOf(parseInt(colRef.current.value)) === -1) {
-      setColorArray((prev) => [...prev, parseInt(colRef.current.value)]);
+
+    setInputError(null);
+
+    if (colorArray.indexOf(cellNumber) === -1) {
+      setColorArray((prev) => [...prev, cellNumber]);
     }
+  };
+
+  const onClearMe = (e) => {
+    e.preventDefault();
+
+    // Reset the states to initial values
+    setColorArray([]);
+    setInputError(null);
+    colRef.current.value = '';
   };
 
   return (
     <div className={styles.color}>
       <form className={styles.form}>
         <input type="number" className={styles.numberInput} ref={colRef} />
+        {inputError && <p className={styles.error}>{inputError}</p>}
         <div className="buttons">
           <input type="submit" value="Color Me" onClick={onColorMe} />
-          <button className={styles.clearBtn} onClick={() => setColorArray([])}>
+          <button className={styles.clearBtn} onClick={onClearMe}>
             Clear Me
           </button>
         </div>
