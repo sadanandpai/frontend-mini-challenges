6f40b76476769bce57e4acd8cd42fb975fdd4a84
Gokulnath N <gnl1cob@bosch.com>
2023-11-19 09:47:23 +0530

    implemented helper function for generating leaderboard data

--- apps/host/src/main.tsx
+++ apps/host/src/main.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./Home";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './pages/home';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>
--- apps/host/src/pages/home.tsx
+++ apps/host/src/pages/home.tsx
@@ -1,11 +1,11 @@
-import Contribution from "@/components/contribution/Contribution";
-import Features from "@/components/features/features";
-import Footer from "@/components/footer/Footer";
-import Hero from "@/components/hero/Hero";
-import Navbar from "@/components/navbar/Navbar";
-import Testimonials from "@/components/testimonials/testimonials";
-import ScrollBtn from "@/components/scroll-to-top/ScrollBtn";
-import styles from "./styles.module.scss";
+import Contribution from '@/components/contribution/Contribution';
+import Features from '@/components/features/features';
+import Footer from '@/components/footer/Footer';
+import Hero from '@/components/hero/Hero';
+import Navbar from '@/components/navbar/Navbar';
+import Testimonials from '@/components/testimonials/testimonials';
+import ScrollBtn from '@/components/scroll-to-top/ScrollBtn';
+import styles from '@/styles.module.scss';
 
 function Home() {
   return (
