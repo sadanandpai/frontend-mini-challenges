bf0e7d3fd17a051528108edd08f3ad7ed5d5f2c8
Sadanand Pai <sadypai@gmail.com>
2023-11-22 09:17:22 +0530

    Contributors list from API

--- react/src/components/contribution/Contribution.tsx
+++ react/src/components/contribution/Contribution.tsx
@@ -4,10 +4,25 @@ import Contributor from './Contributor';
 import { contributorsList } from './contributors-list';
 import styles from './contribution.module.scss';
 import { useAutoAnimate } from '@formkit/auto-animate/react';
+import axios from 'axios';
+
+async function getContributors() {
+  const response = await axios.get(
+    'https://api.github.com/repos/sadanandpai/frontend-mini-challenges/contributors?per_page=1000'
+  );
+  return response.data.map((contributor) => ({
+    username: contributor.login,
+    avatar: contributor.avatar_url?.match(/\d+/)[0],
+  }));
+}
 
 function Contribution() {
   const [contributors, setContributors] = useState(contributorsList);
   const [parent] = useAutoAnimate();
+
+  useEffect(() => {
+    getContributors().then(setContributors);
+  }, []);
 
   useEffect(() => {
     const timer = setInterval(
