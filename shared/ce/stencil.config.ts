f1d007837f64d7fa6e2dc54a44ab6c1d350362f0
arpansaha13 <arpansaha1300@gmail.com>
2023-12-08 19:21:11 +0530

    setup stencil

---
+++ shared/ce/stencil.config.ts
@@ -0,0 +1,24 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'fmc-ce',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  testing: {
+    browserHeadless: "new",
+  },
+};
