f687fe6580c3b888ade9905798e19794a147c15d
arpansaha13 <arpansaha1300@gmail.com>
2023-12-08 18:19:53 +0530

    format

--- apps/react/src/challenges/nested-dropdown/App.jsx
+++ apps/react/src/challenges/nested-dropdown/App.jsx
@@ -1,19 +1,17 @@
-import styles from './style.module.css';
-import { dropdownData } from './data/data';
 import DropdownItems from './DropdownItems';
+import { dropdownData } from './data/data';
+import styles from './style.module.css';
 
 const App = () => {
   return (
     <div className={styles.main}>
-
-      <div className={styles.nestedDropdown} >
-        <ul className={styles.dropdownMain} > {
-          dropdownData.map((menu, index) => {
-            const depthLevel = 0;
-            return <DropdownItems items={menu} key={index} depthLevel={depthLevel} />;
-          })
-        } </ul>
-
+      <div className={styles.nestedDropdown}>
+        <ul className={styles.dropdownMain}>
+          {' '}
+          {dropdownData.map((menu, index) => (
+            <DropdownItems items={menu} key={index} depthLevel={0} />
+          ))}{' '}
+        </ul>
       </div>
     </div>
   );
--- apps/react/src/challenges/nested-dropdown/Dropdown.jsx
+++ apps/react/src/challenges/nested-dropdown/Dropdown.jsx
@@ -1,21 +1,16 @@
 import DropdownItems from './DropdownItems';
 import styles from './style.module.css';
 
-const Dropdown = ({
-    subItems,
-    dropdown,
-    depthLevel
-}) => {
-    depthLevel = depthLevel + 1;
-    const dropdownClass = depthLevel > 1 ? styles.dropdownSubItems : "";
-    return (
-        <ul className={`${styles.dropdown} ${dropdownClass} ${dropdown ? styles.show : ""}`}>
-            {subItems.map((subItem, index) =>
-            (<DropdownItems items={subItem} key={index} depthLevel={depthLevel} />
-            ))
-            }
-        </ul>
-    );
+const Dropdown = ({ subItems, dropdown, depthLevel }) => {
+  const dropdownClass = depthLevel > 1 ? styles.dropdownSubItems : '';
+
+  return (
+    <ul className={`${styles.dropdown} ${dropdownClass} ${dropdown ? styles.show : ''}`}>
+      {subItems.map((subItem, index) => (
+        <DropdownItems items={subItem} key={index} depthLevel={depthLevel + 1} />
+      ))}
+    </ul>
+  );
 };
 
 export default Dropdown;
