11c8a53ed51203abc33096666d59ed82e51c1af4
Sadanand Pai <sadypai@gmail.com>
2023-12-17 08:01:11 +0000

    build files - github action

--- docs/javascript/assets/index-Gkjz8SkH.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-9p4a8sJU.js";import"./header-Xxy6fXYK.js";import"./challenges-Uq9k6NgB.js";import"./navbar--MtIQtyW.js";const e=document.querySelector(".burger-menu"),t=document.querySelector(".nav-links");e.addEventListener("click",()=>{t.classList.toggle("active")});
---
+++ docs/javascript/assets/index-p3Z5p9jW.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-9p4a8sJU.js";import"./header-cWjYD_mj.js";import"./challenges-7VikDAak.js";import"./navbar--p6qScz9.js";const t=document.getElementById("showDialog"),o=document.getElementById("dialogPopup"),e=document.getElementById("closeDialog");t.addEventListener("click",()=>{o.showModal()});e.addEventListener("click",()=>{o.close("")});
