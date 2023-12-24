168e255c3a1937dbfb4d5d1e5018b43939370920
Himanshu <tanwarhimanshu1603@gmail.com>
2023-12-24 12:41:31 +0530

    Added self closing tag in the div having no children

--- apps/react/src/challenges/quote-generator/App.tsx
+++ apps/react/src/challenges/quote-generator/App.tsx
@@ -36,7 +36,7 @@ const App = () => {
   };
 
   if (!quote) {
-    return <div className={styles['loader']}></div>; //Load while the quote is being fetched
+    return <div className={styles['loader']} />; //Load while the quote is being fetched
   }
 
   return (
