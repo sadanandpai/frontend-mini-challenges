634d8785c47dff5586d64c19ef5cd0c575f689ff
sadakchap <aliceprerna@gmail.com>
2024-01-03 23:42:36 +0530

    remove console.log

--- apps/host/src/pages/home.tsx
+++ apps/host/src/pages/home.tsx
@@ -9,7 +9,6 @@ import styles from '@/styles.module.scss';
 import { HashLink } from 'react-router-hash-link';
 
 function HomePage() {
-  console.log('home page....');
   return (
     <>
       <Navbar>
