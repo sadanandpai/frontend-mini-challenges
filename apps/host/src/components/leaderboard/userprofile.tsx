4c1a643cbbc9b6d020277d10af947af322abc16a
Raja Sekhar Thammineni <70139521+raja-sekhar-thammineni@users.noreply.github.com>
2024-01-04 08:57:49 +0000

    Fix: Deep source issue addressed

--- apps/host/src/components/leaderboard/userprofile.tsx
+++ apps/host/src/components/leaderboard/userprofile.tsx
@@ -4,6 +4,22 @@ import classes from "./leaderboard.module.css";
 import { LeaderboardEntry, generateLeaderboardData } from "@/helpers/generate-leaderboard";
 import { IChallenge } from "../../../../../shared/data/types/challenge";
 
+const ChallengesContainer = ({ challenges }: { challenges: IChallenge[] }) => <div className={"challengesCOntainer"}>
+    <h2>
+        List of Contributions
+    </h2>
+    <ul>
+        {challenges?.map((challenge, index) => <a href={`/frontend-mini-challenges/react/#/${challenge.link}`} key={`${challenge.link }-${index}`}> <li>{challenge.title}</li></a>)}
+    </ul>
+</div>
+const ProfileContainer = ({ userProfileDetails }: { userProfileDetails: LeaderboardEntry }) => <div className={classes.profileDetailsContainer}>
+    <div className={classes.profileDetailsContainerLeft}><img src={userProfileDetails.pic} className={classes.userProfileImg} alt={"contributor"} /></div>
+    <div className={classes.profileDetailsContainerRight}>
+        <h3>Name : <span className={classes.fontWeightNormal}>{userProfileDetails.name}</span></h3>
+        <h3>Github Id :  <span className={classes.fontWeightNormal}>{userProfileDetails.developer}</span></h3>
+        <h3>Total Contributions : <span className={classes.fontWeightNormal}>{userProfileDetails.numberOfContributions}</span></h3>
+    </div>
+</div>
 export const UserProfile = () => {
     const { githubid } = useParams();
     const userProfileDetails: LeaderboardEntry = generateLeaderboardData().get(githubid as string) as LeaderboardEntry;
@@ -12,26 +28,8 @@ export const UserProfile = () => {
         <h2 id="user-profile" className={classes.userProfileHeading}>
             User Profile
         </h2>
-        <div className={classes.profileDetailsContainer}>
-            <div className={classes.profileDetailsContainerLeft}><img src={userProfileDetails.pic} className={classes.userProfileImg} alt={"Contributor image"} /></div>
-            <div className={classes.profileDetailsContainerRight}>
-                <h3>Name : <span className={classes.fontWeightNormal}>{userProfileDetails.name}</span></h3>
-                <h3>Github Id :  <span className={classes.fontWeightNormal}>{userProfileDetails.developer}</span></h3>
-                <h3>Total Contributions : <span className={classes.fontWeightNormal}>{userProfileDetails.numberOfContributions}</span></h3>
-            </div>
-        </div>
-        <div className={"challengesCOntainer"}>
-            <h2>
-                List of Contributions
-            </h2>
-            <ul>
-                {
-                    challenges?.map((challenge) =>
-                        <a href={`/frontend-mini-challenges/react/#/${challenge.link}`} > <li>{challenge.title}</li></a>
+        <ProfileContainer userProfileDetails={userProfileDetails} />
+        <ChallengesContainer challenges={challenges} />
 
-                    )
-                }
-            </ul>
-        </div>
     </div>
 }
