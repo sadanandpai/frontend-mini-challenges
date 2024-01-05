5d57f9ac5deb55317b30d5ad6b8caf588eb7587d
LIZA1608 <liza1608.be20@chitkara.edu.in>
2024-01-05 20:35:11 +0530

    updated calculator

--- apps/react/src/challenges/calculator/App.jsx
+++ apps/react/src/challenges/calculator/App.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import styles from "./module.css"
+import styles from "./Calculator.module.css"
 
 export const Calculator = () => {
     const [ first, setFirst ] = useState("")
@@ -170,42 +170,42 @@ export const Calculator = () => {
     }
 
     return (
-        <>
-        <div className="calculator__body">
-            <div className="calculator__screen">
-                <div className="calculator__screen__previous">
+        <div class={styles.container}>
+        <div className={styles.calculator__body}>
+            <div className={styles.calculator__screen}>
+                <div className={styles.calculator__screen__previous}>
                    {equationDisplay()}
                 </div>
-                <div className="calculator__screen__first">
+                <div className={styles.calculator__screen__first}>
                     {result}
                 </div>
             </div>
-            <div className="calculator__buttons">
-                <button onClick={() => clear()} id="ac">Clear</button>
-                <button onClick={() => deleteNumber()} id="del">Del</button>
-                <button onClick={() => negative()} className="calculator__operator" id="negative">+-</button>
-                <button onClick={() => setOperation('^2')} className="calculator__operator" id="square">x<sup>2</sup></button>
-                <button onClick={() => setOperation('^')} className="calculator__operator" id="exponent">x<sup>y</sup></button>
-                <button onClick={() => setOperation('√')} className="calculator__operator" id="radical">√</button>
-                <button onClick={() => setOperation('/')} className="calculator__operator">÷</button>
-                <button onClick={() => setNumber('7')} className="calculator__button">7</button>
-                <button onClick={() => setNumber('8')} className="calculator__button">8</button>
-                <button onClick={() => setNumber('9')} className="calculator__button">9</button>
-                <button onClick={() => setOperation('-')} className="calculator__operator">–</button>
-                <button onClick={() => setNumber('4')} className="calculator__button">4</button>
-                <button onClick={() => setNumber('5')} className="calculator__button">5</button>
-                <button onClick={() => setNumber('6')} className="calculator__button">6</button>
-                <button onClick={() => setOperation('*')} className="calculator__operator">*</button>
-                <button onClick={() => setNumber('1')} className="calculator__button">1</button>
-                <button onClick={() => setNumber('2')} className="calculator__button">2</button>
-                <button onClick={() => setNumber('3')} className="calculator__button">3</button>
-                <button onClick={() => setOperation('+')} className="calculator__operator">+</button>
-                <button onClick={() => setNumber('0')} className="calculator__button">0</button>
-                <button onClick={() => setNumber('.')} className="calculator__button">.</button>
-                <button onClick={() => calculate()} id="total">=</button>
+            <div className={styles.calculator__buttons}>
+                <button onClick={() => clear()} className={styles.calculator__operator}id="ac">Clear</button>
+                <button onClick={() => deleteNumber()} className={styles.calculator__operator}id="del">Del</button>
+                <button onClick={() => negative()} className={styles.calculator__operator} id="negative">+-</button>
+                <button onClick={() => setOperation('^2')} className={styles.calculator__operator} id="square">x<sup>2</sup></button>
+                <button onClick={() => setOperation('^')} className={styles.calculator__operator} id="exponent">x<sup>y</sup></button>
+                <button onClick={() => setOperation('√')} className={styles.calculator__operator} id="radical">√</button>
+                <button onClick={() => setOperation('/')} className={styles.calculator__operator}>÷</button>
+                <button onClick={() => setNumber('7')} className={styles.calculator__button}>7</button>
+                <button onClick={() => setNumber('8')} className={styles.calculator__button}>8</button>
+                <button onClick={() => setNumber('9')} className={styles.calculator__button}>9</button>
+                <button onClick={() => setOperation('-')} className={styles.calculator__operator}>–</button>
+                <button onClick={() => setNumber('4')} className={styles.calculator__button}>4</button>
+                <button onClick={() => setNumber('5')} className={styles.calculator__button}>5</button>
+                <button onClick={() => setNumber('6')} className={styles.calculator__button}>6</button>
+                <button onClick={() => setOperation('*')} className={styles.calculator__operator}>*</button>
+                <button onClick={() => setNumber('1')} className={styles.calculator__button}>1</button>
+                <button onClick={() => setNumber('2')} className={styles.calculator__button}>2</button>
+                <button onClick={() => setNumber('3')} className={styles.calculator__button}>3</button>
+                <button onClick={() => setOperation('+')} className={styles.calculator__operator}>+</button>
+                <button onClick={() => setNumber('0')} className={styles.calculator__button}>0</button>
+                <button onClick={() => setNumber('.')} className={styles.calculator__button}>.</button>
+                <button onClick={() => calculate()} className={styles.calculator__operator} id="total">=</button>
             </div>
         </div>
-        </>
+        </div>
     )
 }
-export default Calculator;
+export default Calculator
