1a813b827029d876090243200fbd401a27cf2e8f
LIZA1608 <liza1608.be20@chitkara.edu.in>
2024-01-06 13:40:35 +0530

    change the layout

--- apps/react/src/challenges/calculator/App.jsx
+++ apps/react/src/challenges/calculator/App.jsx
@@ -1,12 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "./calculator.module.css"
 
 export const Calculator = () => {
-    const [ first, setFirst ] = useState("")
-    const [ second, setSecond ] = useState("")
-    const [ operator, setOperator ] = useState("")
-    const [ result, setResult ] = useState("")
-    const [ equalBtn, setEqualBtn ] = useState(false)
+    const [first, setFirst] = useState("")
+    const [second, setSecond] = useState("")
+    const [operator, setOperator] = useState("")
+    const [result, setResult] = useState("")
+    const [equalBtn, setEqualBtn] = useState(false)
     let number = ""
     let decimalPlaces = ""
 
@@ -43,7 +43,7 @@ export const Calculator = () => {
             setOperator(operator)
         }
     }
-    
+
     // Ensures the correct number of decimal places are returned for floats. 
     const decimalFormat = () => {
         // Changes to string to use .includes()
@@ -85,7 +85,7 @@ export const Calculator = () => {
         } else if (secondString.includes('.')) {
             let secondSplit = secondString.split('.')
             decimalPlaces = secondSplit[1].length
-        } 
+        }
     }
 
     // Sets final result to display, then resets first, second and operator variables. 
@@ -99,20 +99,20 @@ export const Calculator = () => {
 
     const calculate = () => {
         if (operator === '/') {
-            const quotient = parseFloat(first*100)/parseFloat(second*100)
-            returnAndResetEquation(quotient) 
+            const quotient = parseFloat(first * 100) / parseFloat(second * 100)
+            returnAndResetEquation(quotient)
         } else if (operator === '-') {
-            let difference = (parseFloat(first) - parseFloat(second)) *100 // multiplying by 100 allows for no zero to start floats
+            let difference = (parseFloat(first) - parseFloat(second)) * 100 // multiplying by 100 allows for no zero to start floats
             decimalFormat()
-            difference = (difference/100).toFixed(decimalPlaces)
+            difference = (difference / 100).toFixed(decimalPlaces)
             returnAndResetEquation(difference)
         } else if (operator === '*') {
-            let product = first * second  
+            let product = first * second
             decimalFormat()
             product = product.toFixed(decimalPlaces)
             returnAndResetEquation(product)
         } else if (operator === '+') {
-            let sum = parseFloat(first) + parseFloat(second)  
+            let sum = parseFloat(first) + parseFloat(second)
             decimalFormat()
             sum = sum.toFixed(decimalPlaces)
             returnAndResetEquation(sum)
@@ -142,10 +142,10 @@ export const Calculator = () => {
     const deleteNumber = () => {
         if (!operator) {
             const firstString = first.toString()
-            const newFirst = firstString.slice(0, firstString.length-1) 
+            const newFirst = firstString.slice(0, firstString.length - 1)
             setFirst(newFirst)
         } else {
-            const newSecond = second.slice(0, second.length-1)
+            const newSecond = second.slice(0, second.length - 1)
             setSecond(newSecond)
         }
     }
@@ -156,8 +156,8 @@ export const Calculator = () => {
             const negSecond = second * -1
             setSecond(negSecond)
         } else if (first) {
-           const negFirst = first * -1
-           setFirst(negFirst)
+            const negFirst = first * -1
+            setFirst(negFirst)
         }
     }
 
@@ -171,40 +171,40 @@ export const Calculator = () => {
 
     return (
         <div class={styles.container}>
-        <div className={styles.calculator__body}>
-            <div className={styles.calculator__screen}>
-                <div className={styles.calculator__screen__previous}>
-                   {equationDisplay()}
+            <div className={styles.calculator__body}>
+                <div className={styles.calculator__screen}>
+                    <div className={styles.calculator__screen__previous}>
+                        {equationDisplay()}
+                    </div>
+                    <div className={styles.calculator__screen__first}>
+                        {result}
+                    </div>
                 </div>
-                <div className={styles.calculator__screen__first}>
-                    {result}
+                <div className={styles.calculator__buttons}>
+                    <button onClick={() => clear()} id="ac" className={styles.calculator__operator}>Clear</button>
+                    <button onClick={() => deleteNumber()} id="del" className={styles.calculator__operator}>Del</button>
+                    <button onClick={() => negative()} className={styles.calculator__operator} id="negative">+-</button>
+                    <button onClick={() => setOperation('^2')} className={styles.calculator__operator} id="square">x<sup>2</sup></button>
+                    <button onClick={() => setNumber('1')} className={styles.calculator__button}>1</button>
+                    <button onClick={() => setNumber('2')} className={styles.calculator__button}>2</button>
+                    <button onClick={() => setNumber('3')} className={styles.calculator__button}>3</button>
+                    <button onClick={() => setOperation('+')} className={styles.calculator__operator}>+</button>
+                    <button onClick={() => setNumber('4')} className={styles.calculator__button}>4</button>
+                    <button onClick={() => setNumber('5')} className={styles.calculator__button}>5</button>
+                    <button onClick={() => setNumber('6')} className={styles.calculator__button}>6</button>
+                    <button onClick={() => setOperation('/')} className={styles.calculator__operator}>÷</button>
+                    <button onClick={() => setNumber('7')} className={styles.calculator__button}>7</button>
+                    <button onClick={() => setNumber('8')} className={styles.calculator__button}>8</button>
+                    <button onClick={() => setNumber('9')} className={styles.calculator__button}>9</button>
+                    <button onClick={() => setOperation('-')} className={styles.calculator__operator}>–</button>
+                    <button onClick={() => setNumber('0')} className={styles.calculator__button}>0</button>
+                    <button onClick={() => setOperation('^')} className={styles.calculator__operator} id="exponent">x<sup>y</sup></button>
+                    <button onClick={() => setOperation('√')} className={styles.calculator__operator} id="radical">√</button>
+                    <button onClick={() => setOperation('*')} className={styles.calculator__operator}>*</button>
+                    <button onClick={() => setNumber('.')} className={styles.calculator__button}>.</button>
+                    <button onClick={() => calculate()} id="total" className={styles.calculator__operator}>=</button>
                 </div>
             </div>
-            <div className={styles.calculator__buttons}>
-                <button onClick={() => clear()} className={styles.calculator__operator}id="ac">Clear</button>
-                <button onClick={() => deleteNumber()} className={styles.calculator__operator}id="del">Del</button>
-                <button onClick={() => negative()} className={styles.calculator__operator} id="negative">+-</button>
-                <button onClick={() => setOperation('^2')} className={styles.calculator__operator} id="square">x<sup>2</sup></button>
-                <button onClick={() => setOperation('^')} className={styles.calculator__operator} id="exponent">x<sup>y</sup></button>
-                <button onClick={() => setOperation('√')} className={styles.calculator__operator} id="radical">√</button>
-                <button onClick={() => setOperation('/')} className={styles.calculator__operator}>÷</button>
-                <button onClick={() => setNumber('7')} className={styles.calculator__button}>7</button>
-                <button onClick={() => setNumber('8')} className={styles.calculator__button}>8</button>
-                <button onClick={() => setNumber('9')} className={styles.calculator__button}>9</button>
-                <button onClick={() => setOperation('-')} className={styles.calculator__operator}>–</button>
-                <button onClick={() => setNumber('4')} className={styles.calculator__button}>4</button>
-                <button onClick={() => setNumber('5')} className={styles.calculator__button}>5</button>
-                <button onClick={() => setNumber('6')} className={styles.calculator__button}>6</button>
-                <button onClick={() => setOperation('*')} className={styles.calculator__operator}>*</button>
-                <button onClick={() => setNumber('1')} className={styles.calculator__button}>1</button>
-                <button onClick={() => setNumber('2')} className={styles.calculator__button}>2</button>
-                <button onClick={() => setNumber('3')} className={styles.calculator__button}>3</button>
-                <button onClick={() => setOperation('+')} className={styles.calculator__operator}>+</button>
-                <button onClick={() => setNumber('0')} className={styles.calculator__button}>0</button>
-                <button onClick={() => setNumber('.')} className={styles.calculator__button}>.</button>
-                <button onClick={() => calculate()} className={styles.calculator__operator} id="total">=</button>
-            </div>
-        </div>
         </div>
     )
 }
