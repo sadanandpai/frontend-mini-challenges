32634639e9a914c772cdf5aab52e1de3c600ebd5
Sadanand Pai <sadypai@gmail.com>
2024-02-18 16:13:14 +0000

    build files - github action

--- docs/javascript/assets/index-JsJhN6u0.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-9p4a8sJU.js";import"./header-mKzp6dDb.js";import"./ng-challenges-PKAY4EuH.js";import"./navbar-NpshRAZN.js";const t=document.getElementById("showDialog"),o=document.getElementById("dialogPopup"),e=document.getElementById("closeDialog");t.addEventListener("click",()=>{o.showModal()});e.addEventListener("click",()=>{o.close("")});
