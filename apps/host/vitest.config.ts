46be8e85d841523f0bea518ef5addc1c432a3549
Akhil017 <akhil.17.akhi@gmail.com>
2024-03-02 18:23:57 +0530

    added test cases for helper functions in host app

---
+++ apps/host/vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'url';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setup.js',
+    watch: false,
+  },
+});
