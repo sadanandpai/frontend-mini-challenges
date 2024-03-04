3c797126e66186524591c5e02402be06ad3b4bf3
Akshay <62467248+Akshayone8@users.noreply.github.com>
2024-03-04 18:11:38 +0530

    added reply and delete feature

--- apps/react/src/challenges/nested-comments/CommentsList.jsx
+++
@@ -1,6 +0,0 @@
-import CommentsContainer from './CommentsContainer';
-import { CommentsData } from './data/data';
-
-const CommentsList = () => <CommentsContainer commentData={CommentsData} />;
-
-export default CommentsList;
