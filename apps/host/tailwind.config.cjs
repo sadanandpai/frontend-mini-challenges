48bfde45bb1aeb68edd91b0cf7b2876513fbacc7
arpansaha13 <arpansaha1300@gmail.com>
2024-04-24 20:55:49 +0530

    host: setup tailwind

---
+++ apps/host/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{css,tsx}"],
+  darkMode: ["selector", '[data-theme="dark"]'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
