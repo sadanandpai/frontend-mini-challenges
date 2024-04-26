c7caa77a8a4cfb2a51153b815bfac42588e91e05
Sudipta Saha <sudiptasaha@Sudiptas-MacBook-Pro.local>
2024-04-26 11:12:45 +0530

    Feedback Modal- changed to template literals

--- apps/react/src/challenges/feedback-modal/utilities/ClassUtility.js
+++ apps/react/src/challenges/feedback-modal/utilities/ClassUtility.js
@@ -2,6 +2,6 @@ import styles from '../styles.module.css';
 
 export const appendActiveClass = (itemRating, activeRating, usedClasses) => {
   let className = usedClasses;
-  className = activeRating === itemRating ? className + ' ' + styles.active : className;
+  className = activeRating === itemRating ? `${className} ${styles.active}` : `${className}`;
   return className;
 };
