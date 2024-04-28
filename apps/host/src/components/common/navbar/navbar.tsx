9b15462b500ae30b8d6558d6d7b1dd480acca797
Akhil017 <akhil.17.akhi@gmail.com>
2024-04-28 22:57:54 +0530

    hero section, navbar styles migrated to tw

--- apps/host/src/components/common/navbar/navbar.tsx
+++ apps/host/src/components/common/navbar/navbar.tsx
@@ -2,10 +2,10 @@ import { useContext } from "react";
 import React, { useState } from "react";
 import { Drawer, Button } from "antd";
 import { MenuOutlined } from "@ant-design/icons";
-import styles from "./navbar.module.scss";
 import { logo } from "@fmc/assets/images";
 import { ThemeContext } from "../../ThemeWrapper";
-import { Sun, Moon } from "lucide-react";
+import { Sun, Moon, X } from "lucide-react";
+import { cn } from "@/helpers/utils";
 
 function Navbar({
   children,
@@ -29,7 +29,7 @@ function Navbar({
           alt="logo"
           className="h-[26px] w-[26px] md:h-10 md:w-10"
         />
-        <span className="text-center font-medium text-gray-900 md:text-[1.2rem]">
+        <span className="text-center font-medium text-gray-900 md:text-[1.2rem] dark:text-white">
           {title ?? "Frontend Mini Challenges"}
         </span>
       </a>
@@ -56,7 +56,7 @@ function Navbar({
           </svg>
         </a>
         <button
-          className="border-none bg-inherit text-indigo-600 dark:text-indigo-500"
+          className="border-none bg-inherit text-gray-900 dark:text-white"
           onClick={toggleTheme}
         >
           {theme === "light" ? <Sun /> : <Moon />}
@@ -68,55 +68,63 @@ function Navbar({
           type="text"
           icon={<MenuOutlined />}
           onClick={toggleDrawer}
-          style={{
-            color: theme === "light" ? "currentColor" : "#ffffff",
-          }}
+          className="dark:text-white"
         />
       </div>
 
       <Drawer
+        closeIcon={<X className={cn({ "text-white": theme === "dark" })} />}
         title={
-          <span style={{ color: theme === "dark" ? "#fff" : "#000" }}>
-            Menu
-          </span>
+          <div className="flex items-center justify-end gap-4 text-black">
+            <button
+              className={cn("border-none bg-transparent p-0 text-gray-900", {
+                "text-white": theme === "dark",
+              })}
+              onClick={toggleTheme}
+            >
+              {theme === "light" ? <Sun /> : <Moon />}
+            </button>
+            <a
+              href="https://github.com/sadanandpai/frontend-mini-challenges/"
+              target="blank"
+              className={cn("text-gray-900", {
+                "text-white": theme === "dark",
+              })}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
+                />
+              </svg>
+            </a>
+          </div>
         }
         placement="right"
         closable={true}
         onClose={toggleDrawer}
         open={isDrawerOpen}
         width="50%"
-        className={theme === "dark" ? styles.darkDrawer : ""}
         style={{ background: theme === "dark" ? "#192841" : "#fff" }}
       >
-        <div className={styles.drawerContent}>
-          <ul className={styles.drawerList}>
+        <div className="p-4">
+          <ul className="m-0 flex list-none flex-col gap-4 p-0">
             {React.Children.map(children, (child, index) => (
-              <li key={index} className={styles.drawerListItem}>
+              <li
+                key={index}
+                className={cn("[&>a]:text-lg [&>a]:text-black", {
+                  "[&>a]:text-white": theme === "dark",
+                })}
+              >
                 {child}
               </li>
             ))}
           </ul>
-
-          <button className={styles.themeButton} onClick={toggleTheme}>
-            {theme === "light" ? <Sun /> : <Moon />}
-          </button>
-          <a
-            href="https://github.com/sadanandpai/frontend-mini-challenges/"
-            target="blank"
-            className={styles.github}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
-              />
-            </svg>
-          </a>
         </div>
       </Drawer>
     </nav>
--- apps/host/src/components/modules/home/hero/hero.tsx
+++ apps/host/src/components/modules/home/hero/hero.tsx
@@ -1,38 +1,45 @@
-import { coverTransparent, jsImg, reactImg, vueImg, angularImg } from '@fmc/assets/images';
-import styles from './hero.module.scss';
-import { HashLink } from 'react-router-hash-link';
+import {
+  coverTransparent,
+  jsImg,
+  reactImg,
+  vueImg,
+  angularImg,
+} from "@fmc/assets/images";
+import { HashLink } from "react-router-hash-link";
 function Hero() {
   const allImg = [
     {
-      title: 'JS',
+      title: "JS",
       imgSrc: jsImg,
     },
     {
-      title: 'react',
+      title: "react",
       imgSrc: reactImg,
     },
     {
-      title: 'vue',
+      title: "vue",
       imgSrc: vueImg,
     },
     {
-      title: 'angular',
+      title: "angular",
       imgSrc: angularImg,
     },
   ];
   return (
-    <main className={styles.hero}>
+    <main className="flex flex-col justify-between text-center md:flex-row md:text-left">
       <div>
         <h1>
           Prepare for <span>UI coding interviews</span>
         </h1>
 
-        <p>by solving the collection of challenges from Frontend Mini Challenges</p>
+        <p className="mb-8 md:mb-12">
+          by solving the collection of challenges from Frontend Mini Challenges
+        </p>
 
-        <h3 className={styles.link}>
+        <h3 className="[&>a]:inline-block [&>a]:rounded-lg [&>a]:border-[3px] [&>a]:border-solid [&>a]:border-primary [&>a]:bg-white [&>a]:px-8 [&>a]:py-4 [&>a]:font-semibold [&>a]:tracking-widest [&>a]:text-black [&>a]:shadow-[0px_4px_4px_0px_rgba(0,0,0,0.12)] [&>a]:duration-200 [&>a]:hover:shadow-[0px_0px_20px_rgba(0,0,0,0.3)]">
           <HashLink to="javascript">Get Started</HashLink>
         </h3>
-        <div className={styles.heroTechImg}>
+        <div className="flex items-center justify-center gap-3 md:justify-start">
           {allImg.map((item) => (
             <img
               key={item.title}
@@ -45,9 +52,15 @@ function Hero() {
         </div>
       </div>
 
-      <figure className={styles.figure}>
-        <img className={styles.heroImage} src={coverTransparent} alt="brand" />
-        <figcaption>Collection of frontend challenges for learning and interviews</figcaption>
+      <figure className="m-0 flex flex-col items-center justify-center text-center">
+        <img
+          className="aspect-[calc(1120/736)] w-[min(80vh,80vw)] md:w-[min(60vh,60vw)]"
+          src={coverTransparent}
+          alt="brand"
+        />
+        <figcaption>
+          Collection of frontend challenges for learning and interviews
+        </figcaption>
       </figure>
     </main>
   );
