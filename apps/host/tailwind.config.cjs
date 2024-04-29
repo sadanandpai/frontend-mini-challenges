ce36cf87d04995c81631b82d0c81a51ca19d0fd6
Akhil M P <49204264+Akhil017@users.noreply.github.com>
2024-04-29 08:27:46 +0530

    host: hero section and navbar styles migrated to tw (#438)

--- apps/host/tailwind.config.cjs
+++ apps/host/tailwind.config.cjs
@@ -3,7 +3,11 @@ export default {
   content: ["./src/**/*.{css,tsx}"],
   darkMode: ["selector", '[data-theme="dark"]'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(246, 100%, 66%)",
+      },
+    },
   },
   plugins: [],
 };
