46ad8b9a6e40214dfb074a6dc2257777592ef8c3
Akshay <62467248+Akshayone8@users.noreply.github.com>
2024-05-17 20:42:24 +0530

    added tab project (#442)

---
+++ apps/react/src/challenges/tab/data.ts
@@ -0,0 +1,17 @@
+export const tabData = [
+  {
+    label: 'Tab 1',
+    content:
+      'HTML elements tell the browser how to display the content. For example, you can use HTML to create static pages with text, headings, tables, lists, images, links, and more.',
+  },
+  {
+    label: 'Tab 2',
+    content:
+      'Cascading Style Sheets is a style sheet language used for specifying the presentation and styling of a document written in a markup language such as HTML or XML. ',
+  },
+  {
+    label: 'Tab 3',
+    content:
+      'JavaScript, often abbreviated as JS, is a programming language and core technology of the Web, alongside HTML and CSS. 99% of websites use JavaScript on the client side for webpage behavior. Web browsers have a dedicated JavaScript engine that executes the client code. ',
+  },
+];
