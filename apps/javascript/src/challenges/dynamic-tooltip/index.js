4b00e48cad6ca4f703a28ecc5e08ea4763cdfe4e
Bidisha Das <bidishadas@Bidishas-MacBook-Air.local>
2024-06-09 20:00:19 +0530

    fix:Added fix

--- apps/javascript/src/challenges/dynamic-tooltip/index.js
+++
@@ -1,4 +0,0 @@
-var ktooltips = document.querySelectorAll(".avatar");
-ktooltips.forEach(function(ktooltip, index){                // For each ktooltip
-  ktooltip.addEventListener("mouseover", position_tooltip); // On hover, launch the function below
-})
