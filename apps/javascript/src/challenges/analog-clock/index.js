ba693c7f752d2d9b0458e0893dd324d0861262be
Sadanand Pai <sadypai@gmail.com>
2024-07-10 21:11:38 +0530

    JS: Analog clock refinement

--- apps/javascript/src/challenges/analog-clock/index.js
+++ apps/javascript/src/challenges/analog-clock/index.js
@@ -1,31 +1,41 @@
 const clock = document.querySelector('.clock');
+const ticks = document.querySelector('.ticks');
 const hoursHand = document.querySelector('.hours-hand');
 const minutesHand = document.querySelector('.minutes-hand');
 const secondsHand = document.querySelector('.seconds-hand');
 const clockDigitsEl = document.querySelector('.digits');
-const clockDigits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
+const clockDigits = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const ticksCount = 60;
+
+// add ticks to the clock
+for (let i = 0; i < ticksCount; i++) {
+  const tick = document.createElement('div');
+  tick.classList.add('tick');
+  tick.style.left = 50 + Math.sin(((Math.PI * 2) / ticksCount) * i) * 50 + '%';
+  tick.style.top = 50 - Math.cos(((Math.PI * 2) / ticksCount) * i) * 50 + '%';
+  tick.style.transform = `rotate ${i * 6}deg`; // rotate the tick by 6deg for every second
+  ticks.appendChild(tick);
+}
+
+// add digits to the clock
 for (let i = 0; i < clockDigits.length; i++) {
   const digitsEl = document.createElement('div');
   digitsEl.textContent = clockDigits[i];
   digitsEl.classList.add('digit');
-  digitsEl.style.left = 50 + Math.sin(((Math.PI * 2) / clockDigits.length) * i + 0.5) * 45 + '%';
-  digitsEl.style.top = 50 - Math.cos(((Math.PI * 2) / clockDigits.length) * i + 0.5) * 45 + '%';
-
+  digitsEl.style.left = 50 + Math.sin(((Math.PI * 2) / clockDigits.length) * i) * 50 + '%';
+  digitsEl.style.top = 50 - Math.cos(((Math.PI * 2) / clockDigits.length) * i) * 50 + '%';
   clockDigitsEl.appendChild(digitsEl);
 }
 
-const getTimeInAngles = (date) => {
-  return {
-    seconds: (clockDigits.length / 2) * date.getSeconds(),
-    minutes: (clockDigits.length / 2) * date.getMinutes() + date.getSeconds() / 10,
-    hours: date.getMinutes() / 2 + (date.getHours() % clockDigits.length) * 30,
-  };
-};
+const getTimeInAngles = (date) => ({
+  seconds: (clockDigits.length / 2) * date.getSeconds(),
+  minutes: (clockDigits.length / 2) * date.getMinutes() + date.getSeconds() / 10,
+  hours: date.getMinutes() / 2 + (date.getHours() % clockDigits.length) * 30,
+});
 
 const setClockHands = () => {
-  const date = new Date();
-  const angles = getTimeInAngles(date);
+  const angles = getTimeInAngles(new Date());
   hoursHand.style.transform = `rotate(${angles.hours}deg)`;
   minutesHand.style.transform = `rotate(${angles.minutes}deg)`;
   secondsHand.style.transform = `rotate(${angles.seconds}deg)`;
