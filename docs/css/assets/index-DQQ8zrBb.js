2691f21585f2da90c3ba19408962d216250adad5
Sadanand Pai <sadypai@gmail.com>
2024-07-14 11:45:41 +0000

    build files - github action

---
+++ docs/css/assets/index-DQQ8zrBb.js
@@ -0,0 +1,1 @@
+import"./header-CU-GtJFJ.js";import"./navbar-DR12QBpI.js";const t=document.getElementById("showDialog"),o=document.getElementById("dialogPopup"),e=document.getElementById("closeDialog");t.addEventListener("click",()=>{o.showModal()});e.addEventListener("click",()=>{o.close("")});
