5f336f1b3292dbacc5daeca274ae131450f50d04
amanraina1 <amanraina9086@gmail.com>
2024-07-14 22:34:06 +0530

    added speed and words input

--- apps/javascript/src/challenges/multi-typing/index.js
+++ apps/javascript/src/challenges/multi-typing/index.js
@@ -1,11 +1,17 @@
 const text = document.querySelector('.second-text');
+const input = document.querySelector('.input');
+const button = document.querySelector('.button');
+const speed = document.querySelector('.speedRange');
 const values = ['Student', 'Developer', 'Writer'];
+
+speed.default = 200;
+let duration = 200;
 
 function delay(duration) {
   return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
-async function displayTextWithTypingEffect(element, value, duration = 200) {
+async function displayTextWithTypingEffect(element, value) {
   await delay(duration);
   for (let i = 0; i < value.length; i++) {
     element.textContent += value.charAt(i);
@@ -28,3 +34,14 @@ async function textLoad() {
 }
 
 textLoad();
+
+button.addEventListener('click', () => {
+  if (input.value) {
+    values.push(input.value);
+    input.value = '';
+  }
+});
+
+speed.addEventListener('change', () => {
+  duration = speed.value;
+});
