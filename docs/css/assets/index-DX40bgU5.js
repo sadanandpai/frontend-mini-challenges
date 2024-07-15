20642022cd56d9cf15ab32c0ccfb30f4278b4830
Sadanand Pai <sadypai@gmail.com>
2024-07-15 16:51:07 +0000

    build files - github action

---
+++ docs/css/assets/index-DX40bgU5.js
@@ -0,0 +1,1 @@
+import"./header-Cewa_CqA.js";import"./navbar-DTcxwzti.js";const t=document.getElementById("showDialog"),o=document.getElementById("dialogPopup"),e=document.getElementById("closeDialog");t.addEventListener("click",()=>{o.showModal()});e.addEventListener("click",()=>{o.close("")});
---
+++ docs/css/assets/index-RIlbIgRE.js
@@ -0,0 +1,1 @@
+import"./header-Cewa_CqA.js";import"./navbar-DTcxwzti.js";const e=document.getElementById("themeSwitch"),t=document.getElementById("innnerThemeSwitch");e.addEventListener("change",function(){e.checked?(document.body.classList.add("dark"),t.checked=!0):(document.body.classList.remove("dark"),t.checked=!1)});
