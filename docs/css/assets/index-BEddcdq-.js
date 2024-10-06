0536bec8be4de11e5155c4d481ae4a3414ba4a54
Sadanand Pai <sadypai@gmail.com>
2024-10-06 12:43:42 +0000

    build files - github action

--- docs/css/assets/index-BEddcdq-.js
+++
@@ -1,1 +0,0 @@
-import"./header-xSgkrO7l.js";import"./navbar-CeJmf-xG.js";const t=document.getElementById("showDialog"),o=document.getElementById("dialogPopup"),e=document.getElementById("closeDialog");t.addEventListener("click",()=>{o.showModal()});e.addEventListener("click",()=>{o.close("")});
--- docs/css/assets/index-ZghGBLS2.js
+++
@@ -1,1 +0,0 @@
-import"./header-xSgkrO7l.js";import"./navbar-CeJmf-xG.js";const e=document.querySelector(".burger-menu"),t=document.querySelector(".nav-links");e.addEventListener("click",()=>{t.classList.toggle("active")});
