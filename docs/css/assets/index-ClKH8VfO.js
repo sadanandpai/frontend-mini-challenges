0f4da98d69602d1018609b4c67c8aa887211a43a
Sadanand Pai <sadypai@gmail.com>
2024-10-09 04:33:17 +0000

    build files - github action

--- docs/css/assets/index-ClKH8VfO.js
+++
@@ -1,1 +0,0 @@
-import"./header-DAA7Cqsi.js";import"./navbar-FkvA6he0.js";const t=document.getElementById("showDialog"),o=document.getElementById("dialogPopup"),e=document.getElementById("closeDialog");t.addEventListener("click",()=>{o.showModal()});e.addEventListener("click",()=>{o.close("")});
