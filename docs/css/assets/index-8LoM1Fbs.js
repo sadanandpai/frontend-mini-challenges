1070c32e012321999fce2ae4631d2e6d9dd6074c
Sadanand Pai <sadypai@gmail.com>
2024-10-20 16:18:12 +0000

    build files - github action

--- docs/css/assets/index-8LoM1Fbs.js
+++
@@ -1,1 +0,0 @@
-import"./header-DCsdonMe.js";import"./navbar-DycdQRDP.js";const e=document.querySelector(".burger-menu"),t=document.querySelector(".nav-links");e.addEventListener("click",()=>{t.classList.toggle("active")});
