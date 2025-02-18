651a4b171e9bfc8de3aae1a0f3b4df4057c90334
Sadanand Pai <sadypai@gmail.com>
2025-02-18 04:30:47 +0000

    build files - github action

---
+++ docs/css/assets/index-CNkg-0Mt.js
@@ -0,0 +1,1 @@
+import"./header-BMUNWkPp.js";import"./navbar-G1E_9PTK.js";const e=document.getElementById("themeSwitch"),t=document.getElementById("innnerThemeSwitch");e.addEventListener("change",function(){e.checked?(document.body.classList.add("dark"),t.checked=!0):(document.body.classList.remove("dark"),t.checked=!1)});
