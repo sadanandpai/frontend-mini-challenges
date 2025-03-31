0f5d6272ad2704ff0949006137868a9caf03412b
sid <sidrawatkot1@gmail.com>
2025-03-31 16:53:22 +0530

    fixed bugs in react-challenges-stopwatch

--- apps/react/src/challenges/stopwatch/App.jsx
+++ apps/react/src/challenges/stopwatch/App.jsx
@@ -6,44 +6,37 @@ import style from './stopwatch.module.css';
 const App = () => {
   // Create a ref (not to re-render everytime this value changes)
   let animationFrameRef = useRef(0);
-  const lastTimer = useRef(Date.now());
+  const lastTimer = useRef(null);
 
   // local state (changing this value rerenders the UI)
-  const [timerString, updateTime] = useState(['00', '00', '00']);
+  const [time, setTime] = useState(0); // time in ms;
   const [isTimerRunning, setisTimerRunning] = useState(false);
 
   const onStart = () => {
+    lastTimer.current = Date.now() - time;
     setisTimerRunning(true);
     // useRef store it value in ref.value key
     animationFrameRef.current = requestAnimationFrame(timerFn);
   };
 
-  const onStop = () => {
+  const onPause = () => {
     setisTimerRunning(false);
     cancelAnimationFrame(animationFrameRef.current);
   };
 
   const onReset = () => {
     setisTimerRunning(false);
-    updateTime(['00', '00', '00']);
+    setTime(0);
     cancelAnimationFrame(animationFrameRef.current);
-    lastTimer.current = Date.now();
   };
 
   // Function to update the timer values
   const timerFn = () => {
-    // Calculate the elapsed time in milliseconds, seconds, and minutes
+    // Calculate the elapsed time in milliseconds
     const milliSecondElapsed = Date.now() - lastTimer.current;
-    const secondsElapsed = Math.floor(milliSecondElapsed / 1000);
-    const minutesElapsed = Math.floor(secondsElapsed / 60);
-
-    // Convert the elapsed time to strings
-    const milliSeconds = (milliSecondElapsed % 1000).toString().padStart(3, '0');
-    const seconds = (secondsElapsed % 60).toString().padStart(2, '0');
-    const minutes = minutesElapsed.toString().padStart(2, '0');
 
     // updating time state
-    updateTime([minutes, seconds, milliSeconds]);
+    setTime(milliSecondElapsed);
 
     // running the same function (this equal to recursion, but it is improved since browser has control over this fn)
     animationFrameRef.current = requestAnimationFrame(timerFn);
@@ -54,6 +47,17 @@ const App = () => {
     return cancelAnimationFrame(animationFrameRef.current);
   }, []);
 
+  const formatTime = (timeInMs) => {
+    const secondsElapsed = Math.floor(timeInMs / 1000);
+    const minutesElapsed = Math.floor(secondsElapsed / 60);
+
+    // Convert the elapsed time to strings
+    const milliSeconds = (timeInMs % 1000).toString().padStart(3, '0');
+    const seconds = (secondsElapsed % 60).toString().padStart(2, '0');
+    const minutes = minutesElapsed.toString().padStart(2, '0');
+    return `${minutes}:${seconds}:${milliSeconds}`;
+  };
+
   return (
     <div className={style['test']}>
       {/* Watch UI */}
@@ -62,18 +66,18 @@ const App = () => {
           <div className={style['watch-heading']}>Stopwatch</div>
           <div className={style['watch-timer']}>
             {/* Display the timer string */}
-            {`${timerString[0]}:${timerString[1]}:${timerString[2]}`}
+            {formatTime(time)}
           </div>
           <div className={style['watch-btn__container']}>
             <button disabled={isTimerRunning} onClick={onStart} className={style['watch-btn']}>
               Start
             </button>
 
-            <button onClick={onStop} disabled={!isTimerRunning} className={style['watch-btn']}>
-              Stop
+            <button onClick={onPause} disabled={!isTimerRunning} className={style['watch-btn']}>
+              Pause
             </button>
 
-            <button onClick={onReset} disabled={!isTimerRunning} className={style['watch-btn']}>
+            <button onClick={onReset} className={style['watch-btn']}>
               Reset
             </button>
           </div>
