0f756c732fe8b03c8fea52625f46a8e8ae6a4c1b
Tirtha Sharma <tirthagenze121@gmail.com>
2025-05-04 13:49:05 +0530

    Replaced anchor tag with Link tag

--- apps/host/src/components/common/navbar/navbar.tsx
+++ apps/host/src/components/common/navbar/navbar.tsx
@@ -5,6 +5,7 @@ import { logo, logo_dark } from '@fmc/assets/images';
 import { ThemeContext } from '../../ThemeWrapper';
 import { Sun, Moon, Menu } from 'lucide-react';
 import styles from './navbar.module.scss';
+import { Link } from 'react-router-dom';
 
 function Navbar({
   children,
@@ -24,10 +25,10 @@ function Navbar({
 
   return (
     <nav className={styles.navbar}>
-      <a className={styles.logo} href="/frontend-mini-challenges/">
+      <Link className={styles.logo} to="/frontend-mini-challenges/">
         <img src={theme === 'light' ? logo : logo_dark} alt="logo" />
         <span>{title ?? 'Frontend Mini Challenges'}</span>
-      </a>
+      </Link>
 
       <div className={styles.heading}>
         <span>{subheading ?? ''}</span>
@@ -36,8 +37,8 @@ function Navbar({
       <div className={styles.right}>
         <div className={styles.links}>{children}</div>
 
-        <a
-          href="https://github.com/sadanandpai/frontend-mini-challenges/"
+        <Link
+          to="https://github.com/sadanandpai/frontend-mini-challenges/"
           target="blank"
           className={styles.github}
         >
@@ -47,7 +48,7 @@ function Navbar({
               d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
             />
           </svg>
-        </a>
+        </Link>
         <button className={styles.themeButton} onClick={toggleTheme}>
           {theme === 'light' ? <Sun /> : <Moon />}
         </button>
@@ -85,8 +86,8 @@ function Navbar({
           <button className={styles.themeButton} onClick={toggleTheme}>
             {theme === 'light' ? <Sun /> : <Moon />}
           </button>
-          <a
-            href="https://github.com/sadanandpai/frontend-mini-challenges/"
+          <Link
+            to="https://github.com/sadanandpai/frontend-mini-challenges/"
             target="blank"
             className={styles.github}
           >
@@ -96,7 +97,7 @@ function Navbar({
                 d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
               />
             </svg>
-          </a>
+          </Link>
         </div>
       </Drawer>
     </nav>
