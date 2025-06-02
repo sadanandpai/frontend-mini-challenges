1ba1b4543fd6df59ba99d57276409c4dd37d9e7c
Sadanand Pai <sadypai@gmail.com>
2025-06-02 03:48:20 +0000

    build files - github action

---
+++ docs/css/assets/index-Cwc2AqsM.js
@@ -0,0 +1,1 @@
+import"./header-B-Q24_8u.js";import"./navbar-COHsnBh8.js";const e=document.querySelector(".burger-menu"),t=document.querySelector(".nav-links");e.addEventListener("click",()=>{t.classList.toggle("active")});
