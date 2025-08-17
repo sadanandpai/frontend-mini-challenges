7a884b2ed2db5fc764681a8642fe9d49ef4fd2a1
Sadanand Pai <sadypai@gmail.com>
2025-08-17 10:00:55 +0000

    build files - github action

--- docs/css/assets/index-BpsTIsLt.js
+++
@@ -1,1 +0,0 @@
-import"./header-CSOcxSar.js";import"./navbar-DgHAeGoI.js";const e=document.querySelector(".burger-menu"),t=document.querySelector(".nav-links");e.addEventListener("click",()=>{t.classList.toggle("active")});
