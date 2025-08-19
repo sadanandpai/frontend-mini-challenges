6838bb7f20f1c410c43fb3bb670882e6d89b99c9
Sadanand Pai <sadypai@gmail.com>
2025-08-19 15:30:52 +0000

    build files - github action

---
+++ docs/css/assets/index-24eZvUrF.js
@@ -0,0 +1,1 @@
+import"./header-ump-_pIT.js";import"./navbar-Dfa1IajW.js";const e=document.querySelector(".burger-menu"),t=document.querySelector(".nav-links");e.addEventListener("click",()=>{t.classList.toggle("active")});
---
+++ docs/css/assets/index-DFUe6xsJ.js
@@ -0,0 +1,1 @@
+import"./header-ump-_pIT.js";import"./navbar-Dfa1IajW.js";const e=document.getElementById("themeSwitch"),t=document.getElementById("innnerThemeSwitch");e.addEventListener("change",function(){e.checked?(document.body.classList.add("dark"),t.checked=!0):(document.body.classList.remove("dark"),t.checked=!1)});
