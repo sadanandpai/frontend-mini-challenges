a1f927f32d6ba3c1981ba7574e43c9c1bf711327
Pankaj Parkar <parkarpankaj3@gmail.com>
2025-08-22 22:13:42 +0800

    feat: zoneless app correction

--- apps/angular/src/app/app.config.ts
+++ apps/angular/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { routes } from './routes';
@@ -6,7 +6,7 @@ import { routes } from './routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideZoneChangeDetection(),
+    provideZonelessChangeDetection(),
     {
       provide: LocationStrategy,
       useClass: HashLocationStrategy,
